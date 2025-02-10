6abbc77c3d5428c007259ee3693e628f82d0214d
alek <alekviniciuscorreia@gmail.com>
2025-02-10 08:59:50 -0300

    Update R teste.r

--- R teste.r
+++ R teste.r
@@ -33,8 +33,41 @@ OUTRO CODIGO
 
 install.packages(c("httr", "jsonlite", "curl", "xml2"))
 
-library(devtools)
-install_github("Esri/arcgis-r-pkg")
+Loading required package: usethis
+
+Error: Failed to install 'unknown package' from GitHub:
+  HTTP error 404.
+  Not Found
+
+  Did you spell the repo owner (`Esri`) and repo name (`arcgis-r-pkg`) correctly?
+  - If spelling is correct, check that you have the required permissions to access the repo.
+Traceback:
+
+1. pkgbuild::with_build_tools({
+ .     ellipsis::check_dots_used(action = getOption("devtools.ellipsis_action", 
+ .         rlang::warn))
+ .     {
+ .         remotes <- lapply(repo, github_remote, ref = ref, subdir = subdir, 
+ .             auth_token = auth_token, host = host)
+ .         install_remotes(remotes, auth_token = auth_token, host = host, 
+ .             dependencies = dependencies, upgrade = upgrade, force = force, 
+ .             quiet = quiet, build = build, build_opts = build_opts, 
+ .             build_manual = build_manual, build_vignettes = build_vignettes, 
+ .             repos = repos, type = type, ...)
+ .     }
+ . }, required = FALSE)
+2. install_remotes(remotes, auth_token = auth_token, host = host, 
+ .     dependencies = dependencies, upgrade = upgrade, force = force, 
+ .     quiet = quiet, build = build, build_opts = build_opts, build_manual = build_manual, 
+ .     build_vignettes = build_vignettes, repos = repos, type = type, 
+ .     ...)
+3. tryCatch(res[[i]] <- install_remote(remotes[[i]], ...), error = function(e) {
+ .     stop(remote_install_error(remotes[[i]], e))
+ . })
+4. tryCatchList(expr, classes, parentenv, handlers)
+5. tryCatchOne(expr, names, parentenv, handlers[[1L]])
+6. value[[3L]](cond)
+7. stop(remote_install_error(remotes[[i]], e))
 
 install.packages("caminho/para/o/arcgis-r-pkg", repos = NULL, type = "source")
 
