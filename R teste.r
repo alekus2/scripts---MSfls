70464c5bea262d7154650c9b90c124b2a14936a0
alek <alekviniciuscorreia@gmail.com>
2025-02-14 09:13:51 -0300

    Atualizar o R teste.r

--- R teste.r
+++ R teste.r
@@ -5,25 +5,21 @@ alocar_parcelas <- function(tabela_nome) {
   tabela <- arc.open(tabela_nome) # Abre a tabela shapefile
   df <- arc.data2sp(df = tabela) # Converte a tabela em um dataframe
 
-  # Verifica se existe a coluna 'nm_parcela' e se os talhões existem
+  # Verifica se existe a coluna 'nm_parcela'
   if ("nm_parcela" %in% colnames(df)) {
-    # Agrupa por talhão e conta o número de parcelas
-    df_max <- aggregate(nm_parcela ~ talhao, data = df, FUN = max)
-    colnames(df_max)[2] <- "max_parcelas"
+    # Verifica a existência de talhões
+    if (length(unique(df$talhao)) > 0) {
+      # Cria a nova coluna 'contador' com a lógica de contagem
+      df$contador <- ifelse(df$nm_parcela %% 2 != 0, 1, 0)
 
-    # Cria a nova coluna 'CONTADOR' com base na paridade da parcela
-    df$CONTADOR <- ifelse(df$nm_parcela %% 2 != 0, 1, 0)
+      # Filtra apenas as linhas onde 'contador' é 1
+      df <- df[df$contador != 0, ]
 
-    # Filtra apenas as linhas onde CONTADOR é 1
-    df <- df[df$CONTADOR != 0, ]
-
-    # Escreve a nova tabela no arquivo shapefile
-    arc.write(tabela_nome, df, overwrite = TRUE)
-
-    # Cria uma nova tabela chamada 'contador' com os dados desejados
-    contador <- data.frame(talhao = df$talhao, contador = df$CONTADOR)
-    arc.write("contador.shp", contador, overwrite = TRUE) # Salva a tabela contador
-
+      # Escreve o dataframe atualizado de volta no arquivo shapefile
+      arc.write(tabela_nome, df, overwrite = TRUE)
+    } else {
+      stop("Nenhum talhão encontrado na tabela.")
+    }
   } else {
     stop("A coluna 'nm_parcela' não existe na tabela.")
   }
