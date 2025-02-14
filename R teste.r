605ff24b704eb5013b81d55cbd837c2c74ce7c5c
alek <alekviniciuscorreia@gmail.com>
2025-02-14 11:00:44 -0300

    Update R teste.r

--- R teste.r
+++ R teste.r
@@ -1,3 +1,23 @@
 > alocar_parcelas("Pto_Parcelas_QLD.shp")
 Error in alocar_parcelas("Pto_Parcelas_QLD.shp") : 
   could not find function "alocar_parcelas"
+
+library(arcgisbinding)
+alocar_parcelas <- function(tabela_nome) {
+  arc.check_product()
+  tabela <- arc.open(tabela_nome)
+  df <- arc.data2sp(df = tabela)
+  if ("nm_parcela" %in% colnames(df)) {
+    if (length(unique(df$talhao)) > 0) {
+      df$contador <- ifelse(df$nm_parcela %% 2 != 0, 1, 0)
+      df <- df[df$contador != 0, ]
+      arc.write(tabela_nome, df, overwrite = TRUE)
+    } else {
+      stop("Nenhum talhão encontrado na tabela.")
+    }
+  } else {
+    stop("A coluna 'nm_parcela' não existe na tabela.")
+  }
+}
+
+alocar_parcelas("Pto_Parcelas_QLD.shp")
