a28913004f189f151ae79890bcac511e2f486159
alek <alekviniciuscorreia@gmail.com>
2025-02-17 09:16:43 -0300

    Update R teste.r

--- R teste.r
+++ R teste.r
@@ -1,31 +1,24 @@
-if (!requireNamespace("arcgisbinding", quietly = TRUE)) {
-  install.packages("arcgisbinding", repos="https://r.esri.com")
-}
+> source("F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/bds/teste/teste.R", echo=TRUE)
 
-library(arcgisbinding)
-arc.check_product()
+> if (!requireNamespace("arcgisbinding", quietly = TRUE)) {
++   install.packages("arcgisbinding", repos="https://r.esri.com")
++ }
 
-alocar_parcelas <- function(tabela_nome) {
-  arc.check_product()
-  tabela <- arc.open(tabela_nome)
-  df <- arc.select(tabela)
-  
-  if ("NM_PARCELA" %in% colnames(df) && nrow(df) > 0) {
-    df$nm_parcela <- as.numeric(as.character(df$NM_PARCELA))
-    
-    df$contador <- ifelse(df$nm_parcela %% 2 != 0, 1, 0)
-    df <- df[df$contador != 0, ]
-    
-    if (nrow(df) == 0) {
-      stop("Após o filtro, não há linhas restantes no dataframe.")
-    }
-    
-    df_spatial <- arc.data2sp(df)
-    arc.write(tabela_nome, df_spatial, overwrite = TRUE)
-    
-  } else {
-    stop("A coluna 'NM_PARCELA' não existe ou está vazia.")
-  }
-}
+> library(arcgisbinding)
 
-alocar_parcelas("Pto_Qualidade_Parcelas_Piracicaba.shp")
+> arc.check_product()
+product: ArcGIS Pro (12.9.5.32739)
+license: Advanced
+version: 1.0.1.311 
+
+> alocar_parcelas <- function(tabela_nome) {
++   arc.check_product()
++   tabela <- arc.open(tabela_nome)
++   df <- arc.select(tabela)
++   
++   if ("NM ..." ... [TRUNCATED] 
+
+> alocar_parcelas("Pto_Qualidade_Parcelas_Piracicaba.shp")
+Error in validObject(.Object) : 
+  invalid class “SpatialPointsDataFrame” object: number of rows in data.frame and SpatialPoints don't match
+> 
