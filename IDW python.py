9719b95c11f647cb2a62c02e629099679333b9f1
alek <alekviniciuscorreia@gmail.com>
2025-02-21 10:38:15 -0400

    Atualizar o IDW python.py

--- IDW python.py
+++ IDW python.py
@@ -4,80 +4,77 @@ import os
 
 class Toolbox(object):
     def __init__(self):
+        """Define a toolbox."""
         self.label = "Interpolation Processing Toolbox"
         self.alias = "idwpython_toolbox"
-        self.tools = [IDWTool]
+        self.tools = [IDWToolbox]
 
-class IDWTool(object):
+class IDWToolbox(object):
     def __init__(self):
         self.label = "IDW Toolbox com Shapefile"
         self.description = "Executa interpolação IDW usando um Shapefile como entrada."
-        self.canRunInBackground = False
 
     def getParameterInfo(self):
-        params = []
-
-        param1 = arcpy.Parameter(
-            displayName="Shapefile de Entrada",
-            name="input_shp",
-            datatype="DEFeatureClass",
-            parameterType="Required",
-            direction="Input"
-        )
-
-        param2 = arcpy.Parameter(
-            displayName="Pasta de Saída",
-            name="output_folder",
-            datatype="DEFolder",
-            parameterType="Required",
-            direction="Input"
-        )
-
-        param3 = arcpy.Parameter(
-            displayName="Power",
-            name="power",
-            datatype="GPDouble",
-            parameterType="Required",
-            direction="Input"
-        )
-
-        param4 = arcpy.Parameter(
-            displayName="Cell Size",
-            name="cell_size",
-            datatype="GPDouble",
-            parameterType="Required",
-            direction="Input"
-        )
-
-        param5 = arcpy.Parameter(
-            displayName="Layer de Recorte (Opcional)",
-            name="mask_layer",
-            datatype="DEFeatureClass",
-            parameterType="Optional",
-            direction="Input"
-        )
-
-        params.extend([param1, param2, param3, param4, param5])
+        params = [
+            arcpy.Parameter(displayName="Shapefile de Entrada",
+                            name="input_shp",
+                            datatype="DEFeatureClass",
+                            parameterType="Required",
+                            direction="Input"),
+            arcpy.Parameter(displayName="Pasta de Saída",
+                            name="output_folder",
+                            datatype="DEFolder",
+                            parameterType="Required",
+                            direction="Input"),
+            arcpy.Parameter(displayName="Power",
+                            name="power",
+                            datatype="GPDouble",
+                            parameterType="Required",
+                            direction="Input"),
+            arcpy.Parameter(displayName="Cell Size",
+                            name="cell_size",
+                            datatype="GPDouble",
+                            parameterType="Required",
+                            direction="Input"),
+            arcpy.Parameter(displayName="Layer de Recorte (Opcional)",
+                            name="mask_layer",
+                            datatype="DEFeatureClass",
+                            parameterType="Optional",
+                            direction="Input")
+        ]
         return params
 
-    def execute(self, parameters, _):
+    def execute(self, parameters, messages):
         input_shp = parameters[0].valueAsText
         output_folder = parameters[1].valueAsText
         power = parameters[2].value
         cell_size = parameters[3].value  
         mask_layer = parameters[4].valueAsText
 
-        arcpy.MakeFeatureLayer_management(input_shp, "shp_layer_temp")
+        field_names = [f.name for f in arcpy.ListFields(input_shp)]
+        required_fields = ["Name", "Parcela", "F_Sobreviv"]
+
+        arcpy.MakeFeatureLayer_management(input_shp, "shp_layer")
+
         arcpy.CheckOutExtension("spatial")
 
         raster_output_folder = os.path.join(output_folder, "Rasters")
-        os.makedirs(raster_output_folder, exist_ok=True)
+        if not os.path.exists(raster_output_folder):
+            os.makedirs(raster_output_folder)
 
-        out_raster = Idw("shp_layer_temp", "F_Sobreviv", cell_size, power)
-
+        out_raster = arcpy.sa.Idw("shp_layer", "F_Sobreviv", cell_size, power)
+    
         if mask_layer:
             out_raster = ExtractByMask(out_raster, mask_layer)
+                
+        raster_output_path = os.path.join(raster_output_folder, "IDW_Interpolacao.tif")
+        out_raster.save(raster_output_path)
 
-        out_raster.save(os.path.join(raster_output_folder, "IDW_Interpolacao.tif"))
+        symbology = arcpy.sa.RasterClassifySymbology()
+        symbology.valueField = "Value"
+        symbology.breakCount = 5
+        symbology.colorRamp = "Yellow to Red"
+
+        arcpy.ApplySymbologyFromRasterClassify(out_raster, symbology)
 
         arcpy.CheckInExtension("spatial")
