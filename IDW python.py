00332ab1e44a5e3addaa7f494e5338e386592d72
alek <alekviniciuscorreia@gmail.com>
2025-02-24 08:28:41 -0400

    Atualizar o IDW python.py

--- IDW python.py
+++ IDW python.py
@@ -45,7 +45,7 @@ class IDWToolbox(object):
         return params
 
     def execute(self, parameters, messages=None):
-        """Executa a interpolação IDW com recorte e gera um PNG compatível."""
+        """Executa a interpolação IDW com conversão de valores para porcentagem."""
 
         # Recebendo os parâmetros
         input_shp = parameters[0].valueAsText
@@ -57,15 +57,45 @@ class IDWToolbox(object):
         # Ativar licença de extensão Spatial Analyst
         arcpy.CheckOutExtension("Spatial")
 
-        # Criando uma camada de feição temporária
+        # Criando camada de feição temporária
         shp_layer = "shp_layer"
         arcpy.MakeFeatureLayer_management(input_shp, shp_layer)
 
-        # Executando a interpolação IDW
-        field_name = "F_Sobreviv"  # Alterar para o campo correto do shapefile
-        out_raster = Idw(shp_layer, field_name, cell_size, power)
+        # Verificar se o campo F_Sobreviv existe
+        field_name = "F_Sobreviv"
+        field_list = [f.name for f in arcpy.ListFields(input_shp)]
+        if field_name not in field_list:
+            raise Exception(f"O campo '{field_name}' não foi encontrado no Shapefile.")
 
-        # Aplicação da máscara (recorte) se especificado
+        # Criar campo temporário para armazenar os valores em porcentagem
+        percent_field = "F_Sobreviv_Percent"
+
+        # Se o campo já existir, removê-lo
+        if percent_field in field_list:
+            arcpy.DeleteField_management(input_shp, percent_field)
+
+        # Criar o novo campo como Double
+        arcpy.AddField_management(input_shp, percent_field, "DOUBLE")
+
+        # Atualizar os valores para porcentagem
+        with arcpy.da.UpdateCursor(input_shp, [field_name, percent_field]) as cursor:
+            for row in cursor:
+                row[1] = row[0] * 100  # Convertendo para porcentagem
+                cursor.updateRow(row)
+
+        # **Verificar e reprojetar o mask_layer, se necessário**
+        if mask_layer:
+            shp_sr = arcpy.Describe(input_shp).spatialReference
+            mask_sr = arcpy.Describe(mask_layer).spatialReference
+            if shp_sr.name != mask_sr.name:
+                mask_layer_reprojected = os.path.join(output_folder, "mask_reprojected.shp")
+                arcpy.Project_management(mask_layer, mask_layer_reprojected, shp_sr)
+                mask_layer = mask_layer_reprojected
+
+        # **Executando IDW usando o campo convertido**
+        out_raster = Idw(shp_layer, percent_field, cell_size, power)
+
+        # **Aplicando máscara (recorte)**
         if mask_layer:
             out_raster = ExtractByMask(out_raster, mask_layer)
 
@@ -77,7 +107,7 @@ class IDWToolbox(object):
         raster_output_path = os.path.join(output_folder, "IDW_Interpolacao.tif")
         out_raster.save(raster_output_path)
 
-        # **Conversão para PNG corrigida**
+        # **Conversão para PNG**
         png_output_path = os.path.join(output_folder, "IDW_Interpolacao.png")
 
         # Converter para 8 bits antes de salvar como PNG
@@ -87,6 +117,9 @@ class IDWToolbox(object):
 
         # Remover temporário
         arcpy.Delete_management(temp_raster)
+
+        # **Remover o campo temporário do shapefile**
+        arcpy.DeleteField_management(input_shp, percent_field)
 
         # Liberar a extensão Spatial Analyst
         arcpy.CheckInExtension("Spatial")
