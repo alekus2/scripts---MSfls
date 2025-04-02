353fb59824c7b1ded2b5a7397c17249858c4d13b
alek <alekviniciuscorreia@gmail.com>
2025-04-02 11:06:44 -0300

    Update cadastroSGF.py

--- cadastroSGF.py
+++ cadastroSGF.py
@@ -88,10 +88,37 @@ class CadastroSGF:
             if colunas_faltando:
                 print(f"As seguintes colunas não foram encontradas: {', '.join(colunas_faltando)}")
 
-cadastrar = CadastroSGF()     
+Processando: /content/Modelo_atual_cadastro.xlsx
+Colunas encontradas no DataFrame: ['ativo', 'região', 'feição pai', 'descrição de uso do solo', 'tipo propriedade', 'id região', 'id projeto', 'projeto', 'localidade', 'talhão', 'ciclo', 'rotação', 'tipo', 'fase', 'bacia', 'solo', 'relevo', 'espaçamento', 'sistema de propagação', 'mat.genético', 'espécie', 'data plantio', 'mês de plantio', 'regime', 'sítio', 'área (ha)', 'área gis', 'atualizar via gis', 'distância total', 'situação', 'cód. projeto investimento', 'dcr. projeto investimento', 'processo comercial', 'processo jurídico', 'organização', 'cód. tarefa proj. invest.', 'fazendas', 'registro', 'id', 'gênero', 'idade', 'manejo', 'distância média baldeio', 'cd uso solo pai', 'planooperacao', 'responsável', 'observações', 'data plantio projetado', 'início vigência', 'fim vigência', 'dcaa data emissão', 'dcaa data validade', 'data primeiro plantio', 'distância terra', 'distância asfalto', 'precipitação', 'ciclo de investimento', '% saída madeira - chuva', 'volume madeira', 'chave', 'tipo contrato', 'projeto expansão', 'dcaa número', 'regional colheita', 'regional silvicultura', 'região climática', 'certificação', 'tipo de licença', 'produto', 'fábrica', 'manejo futuro', 'bioma', 'tipologia', 'coletor de custo']
+Coluna não encontrada: cód. região
+Coluna não encontrada: uso do talhão
+Coluna não encontrada: descrição do uso do talhão
+Coluna não encontrada: plantio
+Coluna não encontrada: sitio
+Coluna não encontrada: área(ha)
+Coluna não encontrada: status
+Coluna não encontrada: id terra potencial
+Coluna não encontrada: observacao
+Coluna não encontrada: tipo de contrato
+Coluna não encontrada: proj. de expansão
+Coluna não encontrada: cod antigo proj.
+Coluna não encontrada: área mrp
+Coluna não encontrada: terra
+Coluna não encontrada: classe
+Coluna não encontrada: flagctovirtual
+---------------------------------------------------------------------------
+KeyError                                  Traceback (most recent call last)
+<ipython-input-5-cdc3712b18e4> in <cell line: 0>()
+     89 ]
+     90 
+---> 91 cadastrar.verificar(arquivos)
 
-arquivos = [
-    r"F:\Qualidade_Florestal\02- MATO GROSSO DO SUL\11- Administrativo Qualidade MS\00- Colaboradores\17 - Alex Vinicius\Automações em python\Automatização_cadastro_SGF\dados\Modelo_atual_cadastr.xlsx"
-]
+3 frames
+/usr/local/lib/python3.11/dist-packages/pandas/core/indexes/base.py in _raise_if_missing(self, key, indexer, axis_name)
+   6250 
+   6251             not_found = list(ensure_index(key)[missing_mask.nonzero()[0]].unique())
+-> 6252             raise KeyError(f"{not_found} not in index")
+   6253 
+   6254     @overload
 
-cadastrar.verificar(arquivos)
+KeyError: "['Início vigência', 'Fim vigência', 'Distância asfalto', 'Regional colheita', 'Regional silvicultura', 'Região climática'] not in index"
