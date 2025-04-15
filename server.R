ef5542d1cea7e2c7f6be38ba4585765db34fefd6
alek <alekviniciuscorreia@gmail.com>
2025-04-15 11:36:27 -0400

    Update server.R

--- server.R
+++ server.R
@@ -5,38 +5,27 @@ server <- function(input, output, session) {
   observeEvent(input$confirmar, {
     output$shape_text <- renderText({
       req(input$shape)
-      paste("Upload realizado referente aos talhıes:", input$shape$name)
+      paste("Upload realizado referente aos talh√µes:", input$shape$name)
     })
     
     output$recomend_text <- renderText({
       req(input$recomend)
-      paste("Upload realizado referente a recomendaÁ„o de parcelas:", input$recomend$name)
+      paste("Upload realizado referente a recomenda√ß√£o de parcelas:", input$recomend$name)
     })
     
     output$parc_exist_text <- renderText({
       if (input$parcelas_existentes_lancar == 1) {
         req(input$parc_exist)
-        paste("Upload realizado referente as parcelas j· existentes:", input$parc_exist$name)
+        paste("Upload realizado referente as parcelas j√° existentes:", input$parc_exist$name)
       } else {
-        paste0("Upload de parcelas existentes n„o realizado.")
+        paste0("Upload de parcelas existentes n√£o realizado.")
       }
     })
     
     output$confirmation <- renderText({
       req(input$forma_parcela, input$tipo_parcela, input$distancia_minima)
-      paste("Forma Parcela:", input$forma_parcela, "Tipo Parcela:", input$tipo_parcela, "Dist‚ncia MinÌma: ", input$distancia_minima)
+      paste("Forma Parcela:", input$forma_parcela, "Tipo Parcela:", input$tipo_parcela, "Dist√¢ncia M√≠nima:", input$distancia_minima)
     })
-  })
-  
-  grid_existente <- reactive({
-  req(input$grid_existente)
-  
-  shp_path <- input$grid_existente$datapath[grepl(".shp$", input$grid_existente$name)]
-  dir_path <- dirname(shp_path)
-  shp_name <- input$grid_existente$name[grepl(".shp$", input$grid_existente$name)]
-  shp_full <- file.path(dir_path, shp_name)
-  
-  st_read(shp_full, options = "ENCODING=UTF-8")
   })
   
   forma_parcela <- reactive({
@@ -49,6 +38,10 @@ server <- function(input, output, session) {
   
   distancia_minima <- reactive({
     input$distancia_minima
+  })
+  
+  intensidade_amostral <- reactive({
+    input$intensidade_amostral
   })
   
   shape_path <- reactive({
@@ -65,52 +58,47 @@ server <- function(input, output, session) {
     if(input$shape_input_pergunta_arudek == 0) {
       shp <- shp %>%
         rename(ID_PROJETO := !!input$mudar_nome_arudek_projeto,
-               ID_TALHAO := !!input$mudar_nome_arudek_talhao,
+               TALHAO := !!input$mudar_nome_arudek_talhao,
                CICLO := !!input$mudar_nome_arudek_ciclo,
                ROTACAO := !!input$mudar_nome_arudek_rotacao)
     }
     
     shp <- shp %>%
       mutate(ID_PROJETO = str_pad(ID_PROJETO, 4, pad = 0),
-             ID_TALHAO = str_pad(ID_TALHAO, 3, pad = 0))
+             TALHAO = str_pad(TALHAO, 3, pad = 0))
     
     return(shp)
   })
   
-  
   parc_exist_path <- reactive({
-    
     if(input$parcelas_existentes_lancar == 1) {
       req(input$parc_exist)
       parc_exist_zip <- unzip(input$parc_exist$datapath, exdir = tempdir())
       grep(parc_exist_zip, pattern = ".shp$", value = TRUE)
-      
     } else {
       paste0("data/parc.shp")
     }
   })
   
   recomend <- reactive({
-    
     if(input$recomendacao_pergunta_upload == 1) {
       req(input$recomend)
       recomends <- read.csv2(input$recomend$datapath) %>% 
         mutate(Projeto = str_pad(Projeto, 4, pad = 0),
                Talhao = str_pad(Talhao, 3, pad = 0),
                Index = paste(Projeto, Talhao, sep = "")) %>%
-        rename(Num.parc = N, ID_PROJETO = Projeto, ID_TALHAO = Talhao)
+        rename(Num.parc = N, ID_PROJETO = Projeto, TALHAO = Talhao)
       return(recomends) 
     } else {
-      
       req(shape(), input$recomend_intensidade)
       shape <- shape() %>% st_make_valid()
       
       recomends <- shape %>% 
-        group_by(ID_PROJETO, ID_TALHAO) %>%
+        group_by(ID_PROJETO, TALHAO) %>%
         summarise(Num.parc = ceiling(sum(st_area(geometry)) / (10000 * as.numeric(input$recomend_intensidade)))) %>%
         mutate(Num.parc = ifelse(as.numeric(Num.parc) < 2, 2, Num.parc),
-               Index = paste0(ID_PROJETO, ID_TALHAO)) %>%
-        select(ID_PROJETO, ID_TALHAO, Num.parc, Index) %>%
+               Index = paste0(ID_PROJETO, TALHAO)) %>%
+        select(ID_PROJETO, TALHAO, Num.parc, Index) %>%
         as.data.frame() %>%
         select(-geometry)
       
@@ -128,23 +116,15 @@ server <- function(input, output, session) {
     contentType = "text/csv"
   )
   
-  
   progress_percentage <- reactiveVal(0)
   values <- reactiveValues(result_points = NULL, process_complete = FALSE)
   
   observeEvent(input$gerar_parcelas, {
     session$sendCustomMessage("hide_completed", message = "")
     
-    
-   
-    result <- process_data(shape = shape(),
-                          recomend = recomend(),
-                          parc_exist_path = caminho_parc_exist(),
-                          forma_parcela = input$formato,
-                          tipo_parcela = input$tipo,
-                          distancia.minima = input$distancia,
-                          update_progress = function(percent) {session$sendCustomMessage("update_progress", percent)},
-                          grid_existente = grid_existente())
+    result <- process_data(shape(), recomend(), parc_exist_path(), forma_parcela(), tipo_parcela(), distancia_minima(), intensidade_amostral(), function(percent) {
+      session$sendCustomMessage("update_progress", percent)
+    })
     
     if(input$lancar_sobrevivencia == 1){
       unique_indexes <- unique(result$Index)
@@ -198,14 +178,13 @@ server <- function(input, output, session) {
       mutate(Index = paste0(PROJETO, TALHAO)) %>%
       filter(Index != selected_index)
     
-    
     session$sendCustomMessage("hide_completed", message = "")
     shape_selected <- shape() %>%
-      mutate( Index = paste0(ID_PROJETO, ID_TALHAO)) %>% 
+      mutate( Index = paste0(ID_PROJETO, TALHAO)) %>% 
       filter(Index == selected_index)
     
     recomend_selected <- recomend() %>% 
-      mutate(Index = paste0(ID_PROJETO, ID_TALHAO)) %>%
+      mutate(Index = paste0(ID_PROJETO, TALHAO)) %>%
       filter(Index == selected_index)
     
     result <- process_data(shape_selected, recomend_selected, parc_exist_path(), forma_parcela(), tipo_parcela(), distancia_minima(), function(percent) {
@@ -277,8 +256,6 @@ server <- function(input, output, session) {
     updateSelectInput(session, "selected_index", selected = indexes_list[prev_index])
   })
   
-  
-
   output$plot <- renderPlot({
     req(values$result_points, input$selected_index, shape())
     selected_index <- input$selected_index
@@ -286,7 +263,7 @@ server <- function(input, output, session) {
     shape_aux <- shape()
     shape_aux <- st_transform(shape_aux, 31982)
     
-    shape_aux$Index <- paste0(shape_aux$ID_PROJETO, shape_aux$ID_TALHAO)
+    shape_aux$Index <- paste0(shape_aux$ID_PROJETO, shape_aux$ITALHAO)
     shape_filtrado <- shape_aux %>% filter(Index == selected_index)
     points_df <- result_aux %>% filter(Index == selected_index)
     
@@ -294,20 +271,23 @@ server <- function(input, output, session) {
       group_by(ID_PROJETO, ID_TALHAO) %>%
       summarise(Num.parc = ceiling(sum(st_area(geometry)) / (10000 * as.numeric(input$recomend_intensidade)))) %>%
       mutate(Num.parc = ifelse(as.numeric(Num.parc) < 2, 2, Num.parc),
-             Index = paste0(ID_PROJETO, ID_TALHAO)) %>%
+             Index = paste0(ID_PROJETO, TALHAO)) %>%
       pull(Num.parc)
     
     area_titulo <- round(sum(st_area(shape_filtrado)) / 10000, 2)
     
     ggplot() + 
       geom_sf(aes(), data = shape_filtrado) + 
-      geom_sf(aes(), data = points_df) + 
-      custom_theme +
-      ggtitle(paste0("N√∫mero de parcelas recomendado: ", num_parc_title, " (√Årea: ", area_titulo, " ha)"))
+      theme_bw() +
+      geom_sf(data = points_df %>% filter(!is.na(LATITUDE)), aes(geometry = geometry)) + 
+      theme(legend.position = "none") +
+      ggtitle(paste0("√Årea (ha): ", area_titulo, "\nNum de Parcelas:", num_parc_title))
   })
+
+
   
-  output$download_result <- downloadHandler(
-    filename = function() {
+    output$download_result <- downloadHandler(
+      filename = function() {
       paste0(input$download_name, ".zip")
     },
     content = function(file) {
