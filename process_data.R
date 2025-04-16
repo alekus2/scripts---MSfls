46bb0a0a3983531bd3e0d0457f4d58b2f6a092cb
alek <alekviniciuscorreia@gmail.com>
2025-04-16 11:05:46 -0400

    Atualizar o process_data.R

--- process_data.R
+++ process_data.R
@@ -7,6 +7,7 @@ process_data <- function(shape, recomend, parc_exist_path, forma_parcela,
   library(tidyr)
   
   parc_exist <- st_read(parc_exist_path)
+  
   shape <- st_transform(shape, 31982)
   parc_exist <- st_transform(parc_exist, 31982)
   
@@ -25,9 +26,11 @@ process_data <- function(shape, recomend, parc_exist_path, forma_parcela,
       shapeb[[i]] <- buffered
     }
   }
+  
   if (!is.null(empty_indexes) && length(empty_indexes) > 0) {
     shapeb <- shapeb[-empty_indexes]
   }
+  
   shapeb <- do.call("rbind", shapeb)
   
   result_points <- list()
@@ -43,91 +46,74 @@ process_data <- function(shape, recomend, parc_exist_path, forma_parcela,
       sg <- subgeoms[i, ]
       sg_area <- as.numeric(st_area(sg))
       
-      if (sg_area < 400) next
+      if (sg_area < 400) {
+        next
+      }
       
       active_points_all <- parc_exist[parc_exist$STATUS == "ATIVA" & parc_exist$Index == poly_idx, ]
       active_points <- st_intersection(st_geometry(active_points_all), st_geometry(sg))
       
       if (sg_area >= 400 & sg_area <= 1000) {
-        if (length(active_points) > 0) next
-        
-        cell.point <- st_centroid(st_geometry(sg))
-        conf.point <- st_buffer(cell.point, dist = sqrt(400 / pi))
-        conf.point <- st_intersection(conf.point, sg) %>% st_sf()
-        
-        points2 <- st_sf(data.frame(
-          Area = sg_area,
-          Index = poly_idx,
-          PROJETO = poly$ID_PROJETO,
-          TALHAO = poly$ID_TALHAO,
-          CICLO = poly$CICLO,
-          ROTACAO = poly$ROTACAO,
-          STATUS = "ATIVA",
-          FORMA = forma_parcela, 
-          TIPO_INSTA = tipo_parcela,
-          TIPO_ATUAL = tipo_parcela, 
-          DATA = Sys.Date(),
-          DATA_ATUAL = Sys.Date(),
-          COORD_X = st_coordinates(cell.point)[1],
-          COORD_Y = st_coordinates(cell.point)[2]
-        ), geometry = st_geometry(cell.point))
-        
-        result_points[[paste(poly_idx, i, sep = "-")]] <- points2
-        
+        if (length(active_points) > 0) {
+          next
+        } else {
+          cell.point <- st_centroid(st_geometry(sg))
+          conf.point <- st_buffer(cell.point, dist = sqrt(400 / pi))
+          conf.point <- st_intersection(conf.point, sg) %>% st_sf()
+          
+          points2 <- st_sf(data.frame(
+            Area = sg_area,
+            Index = poly_idx,
+            PROJETO = poly$ID_PROJETO,
+            TALHAO = poly$ID_TALHAO,
+            CICLO = poly$CICLO,
+            ROTACAO = poly$ROTACAO,
+            STATUS = "ATIVA",
+            FORMA = forma_parcela, 
+            TIPO_INSTA = tipo_parcela,
+            TIPO_ATUAL = tipo_parcela, 
+            DATA = Sys.Date(),
+            DATA_ATUAL = Sys.Date(),
+            COORD_X = st_coordinates(cell.point)[1],
+            COORD_Y = st_coordinates(cell.point)[2]
+          ), geometry = st_geometry(cell.point))
+          
+          result_points[[paste(poly_idx, i, sep = "-")]] <- points2
+        }
       } else {
-        num_parc_recom <- as.numeric(recomend[recomend$Index == poly_idx, "Num.parc"])
-        num_parc_desejado <- round(num_parc_recom * as.numeric(intensidade_amostral))
+        area_por_parcela <- as.numeric(intensidade_amostral) * 10000
+        cell_size <- sqrt(area_por_parcela)
         
-        sg_area_ha <- sg_area / 10000
-        max_possible_plots <- floor(sg_area_ha / as.numeric(intensidade_amostral))
-        num_parc <- min(num_parc_desejado, max_possible_plots)
-        
-        if (num_parc == 0) next
-        
-        # Definir número de linhas e colunas para a malha sistemática
-        num_cols <- ceiling(sqrt(num_parc))
-        num_rows <- ceiling(num_parc / num_cols)
-        
-        bbox <- st_bbox(sg)
-        cell_width <- (bbox$xmax - bbox$xmin) / num_cols
-        cell_height <- (bbox$ymax - bbox$ymin) / num_rows
-        
-        grid <- st_make_grid(sg, cellsize = c(cell_width, cell_height), what = "polygons", square = TRUE)
+        grid <- st_make_grid(sg, cellsize = c(cell_size, cell_size), what = "polygons", square = TRUE)
         grid <- st_intersection(grid, sg) %>% st_sf()
         grid$area.grid <- round(as.numeric(st_area(grid)))
-        grid <- grid %>% filter(area.grid >= (cell_width * cell_height * 0.7))
+        grid <- grid %>% filter(area.grid >= (cell_size^2 * 0.6))
         
-        if(nrow(grid) == 0) next
+        grid_centroids <- st_centroid(st_geometry(grid))
         
-        grid_selecionado <- head(grid, num_parc)
+        if (length(active_points) > 0) {
+          grid_centroids <- grid_centroids[st_is_within_distance(grid_centroids, active_points, dist = distancia.minima, sparse = FALSE)[,1] == FALSE]
+        }
+        
         points_list <- list()
-        
-        for (j in 1:nrow(grid_selecionado)) {
-          cell <- grid_selecionado[j, ]
-          active_points_cell <- st_intersection(st_geometry(active_points), st_geometry(cell))
-          
-          if (length(active_points_cell) == 0) {
-            cell.point <- st_centroid(st_geometry(cell))
-            area_vector <- as.numeric(st_area(cell))
-            index_vector <- rep(poly_idx, length(area_vector))
-            
-            points_list[[j]] <- st_sf(data.frame(
-              Area = area_vector,
-              Index = index_vector,
-              PROJETO = poly$ID_PROJETO,
-              TALHAO = poly$ID_TALHAO,
-              CICLO = poly$CICLO,
-              ROTACAO = poly$ROTACAO,
-              STATUS = "ATIVA",
-              FORMA = forma_parcela, 
-              TIPO_INSTA = tipo_parcela,
-              TIPO_ATUAL = tipo_parcela,
-              DATA = Sys.Date(),
-              DATA_ATUAL = Sys.Date(),
-              COORD_X = st_coordinates(cell.point)[1],
-              COORD_Y = st_coordinates(cell.point)[2]
-            ), geometry = st_geometry(cell.point))
-          }
+        for (j in seq_along(grid_centroids)) {
+          cell.point <- grid_centroids[j]
+          points_list[[j]] <- st_sf(data.frame(
+            Area = area_por_parcela,
+            Index = poly_idx,
+            PROJETO = poly$ID_PROJETO,
+            TALHAO = poly$ID_TALHAO,
+            CICLO = poly$CICLO,
+            ROTACAO = poly$ROTACAO,
+            STATUS = "ATIVA",
+            FORMA = forma_parcela, 
+            TIPO_INSTA = tipo_parcela,
+            TIPO_ATUAL = tipo_parcela, 
+            DATA = Sys.Date(),
+            DATA_ATUAL = Sys.Date(),
+            COORD_X = st_coordinates(cell.point)[1],
+            COORD_Y = st_coordinates(cell.point)[2]
+          ), geometry = cell.point)
         }
         if (length(points_list) > 0) {
           points2 <- do.call("rbind", points_list)
