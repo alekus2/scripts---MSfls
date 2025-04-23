447fbb0b356c05ff060803b2b43c309fc0ca925f
alek <alekviniciuscorreia@gmail.com>
2025-04-23 10:22:02 -0400

    Atualizar o process_data.R

--- process_data.R
+++ process_data.R
@@ -3,17 +3,13 @@ library(dplyr)
 
 process_data <- function(shape, recomend, parc_exist_path,
                          forma_parcela, tipo_parcela,
-                         distancia.minima,      # em metros, ex: 50
-                         intensidade_amostral,  # X em “1 ponto a cada X ha”
-                         update_progress) {     # callback para progresso
-
-  # 1) lê e reprojeta as parcelas existentes
-  parc_exist <- st_read(parc_exist_path) %>% 
+                         intensidade_proporcao,    # 5 ou 10
+                         update_progress,
+                         distancia_minima = 200) {
+  
+  parc_exist <- st_read(parc_exist_path) %>%
     st_transform(31982)
 
-  # 2) prepara shapefile principal:
-  #    - reprojeta para metros,
-  #    - extrai AREA_HA (já existente) e CD_USO_SOL como Index
   shape_full <- shape %>%
     st_transform(31982) %>%
     mutate(
@@ -21,62 +17,52 @@ process_data <- function(shape, recomend, parc_exist_path,
       AREA_HA = as.numeric(AREA_HA)
     )
 
-  # 3) aplica buffer interno usando o parâmetro distancia.minima
-  buf_dist <- -abs(distancia.minima)
-  shapeb   <- shape_full %>%
-    st_buffer(buf_dist) %>%
+  shapeb <- shape_full %>%
+    st_buffer(-50) %>%
     filter(!st_is_empty(geometry))
-
+  
   result_points <- list()
-  total_poly   <- n_distinct(shapeb$Index)
-  completed    <- 0
+  total_poly    <- n_distinct(shapeb$Index)
+  completed     <- 0
 
-  # 4) itera por cada talhão (Index)
   for (idx in unique(shapeb$Index)) {
-    talhao   <- filter(shapeb, Index == idx)
-    area_ha  <- unique(talhao$AREA_HA)    # em hectares
-    subgeo   <- split_subgeometries(talhao)
+    talhao    <- filter(shapeb, Index == idx)
+    area_ha   <- unique(talhao$AREA_HA)
+    subgeoms  <- split_subgeometries(talhao)
+    
+    for (i in seq_len(nrow(subgeoms))) {
+      sg      <- subgeoms[i, ]
+      area_sg <- as.numeric(st_area(sg))
+      if (area_sg < 400) next
 
-    for (i in seq_len(nrow(subgeo))) {
-      sg      <- subgeo[i, ]
-      area_sg <- as.numeric(st_area(sg))   # em m²
-      if (area_sg < 400) next              # pula pedaços muito pequenos
+      n_req <- max(1, floor(area_ha / intensidade_proporcao))
 
-      # 5a) determina quantos pontos: 1 por intensidade_amostral ha
-      n_req <- max(1, floor(area_ha / intensidade_amostral))
-
-      # 5b) inicia delta para grade
-      delta     <- sqrt(area_sg / n_req)
+      # Geração inicial com grade regular
       bb        <- st_bbox(sg)
-      offset_xy <- c(bb$xmin + delta/2, bb$ymin + delta/2)
+      grid_pts  <- st_make_grid(
+        x        = sg,
+        cellsize = c(distancia_minima, distancia_minima),
+        offset   = c(bb$xmin + distancia_minima/2, bb$ymin + distancia_minima/2),
+        what     = "centers"
+      )
+      inside    <- st_within(grid_pts, sg, sparse = FALSE)
+      pts       <- grid_pts[apply(inside, 1, any)]
 
-      # 5c) ajusta delta dinamicamente para obter ≥ n_req centros
-      for (iter in seq_len(20)) {
-        grid_pts <- st_make_grid(
-          x        = sg,
-          cellsize = c(delta, delta),
-          offset   = offset_xy,
-          what     = "centers"
-        )
-        inside   <- st_within(grid_pts, sg, sparse = FALSE)
-        pts      <- grid_pts[apply(inside, 1, any)]
-
-        if (length(pts) >= n_req) break
-        delta <- delta * 0.95  # aperta grade em 5%
+      # Se houver mais pontos que o necessário, selecionar apenas os mais centrais
+      if (length(pts) > n_req) {
+        cr       <- st_coordinates(pts)
+        center   <- c(mean(range(cr[,1])), mean(range(cr[,2])))
+        dists    <- sqrt((cr[,1] - center[1])^2 + (cr[,2] - center[2])^2)
+        pts      <- pts[order(dists)][1:n_req]
       }
+
       if (length(pts) == 0) next
 
-      # 5d) ordena e seleciona exatamente n_req (ou o máximo que couber)
-      cr      <- st_coordinates(pts)
-      ord     <- order(cr[,1], cr[,2])
-      sel_pts <- pts[ord][ seq_len(min(length(pts), n_req)) ]
-
-      # 5e) monta o sf de saída
-      coords   <- st_coordinates(sel_pts)
+      coords   <- st_coordinates(pts)
       n_found  <- nrow(coords)
       pts_sf   <- st_sf(
         data.frame(
-          Index      = rep(idx,      n_found),
+          Index      = rep(idx,     n_found),
           PROJETO    = rep(talhao$ID_PROJETO, n_found),
           TALHAO     = rep(talhao$TALHAO,    n_found),
           CICLO      = rep(talhao$CICLO,     n_found),
@@ -90,9 +76,9 @@ process_data <- function(shape, recomend, parc_exist_path,
           COORD_X    = coords[,1],
           COORD_Y    = coords[,2]
         ),
-        geometry = sel_pts
+        geometry = pts
       )
-
+      
       result_points[[paste(idx, i, sep = "_")]] <- pts_sf
     }
 
@@ -100,10 +86,8 @@ process_data <- function(shape, recomend, parc_exist_path,
     update_progress(round(completed / total_poly * 100, 2))
   }
 
-  # 6) combina todos os pontos gerados
   all_pts <- do.call(rbind, result_points)
 
-  # 7) numeração sequencial com base em parc_exist
   parcelasinv <- parc_exist %>%
     st_drop_geometry() %>%
     group_by(PROJETO) %>%
@@ -119,7 +103,7 @@ process_data <- function(shape, recomend, parc_exist_path,
       )
     ) %>%
     select(PROJETO, numeracao_inicial)
-
+  
   all_pts <- all_pts %>%
     left_join(parcelasinv, by = "PROJETO") %>%
     replace_na(list(numeracao_inicial = 1)) %>%
