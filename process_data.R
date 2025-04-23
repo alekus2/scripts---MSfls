00866191ea8248841f3cac36b8ad05af6d5dc63f
alek <alekviniciuscorreia@gmail.com>
2025-04-23 08:12:47 -0400

    Atualizar o process_data.R

--- process_data.R
+++ process_data.R
@@ -1,184 +1,122 @@
-process_data <- function(shape, recomend, parc_exist_path, forma_parcela, tipo_parcela,  distancia.minima, intensidade_amostral,update_progress) {
+library(sf)
+library(dplyr)
+
+process_data <- function(shape, recomend, parc_exist_path, 
+                         forma_parcela, tipo_parcela,  
+                         distancia.minima, intensidade_amostral,
+                         update_progress) {
   
-  parc_exist <- st_read(parc_exist_path)
+  # Leitura e transformação das parcelas existentes
+  parc_exist <- st_read(parc_exist_path)  
+  parc_exist <- st_transform(parc_exist, 31982)  # CRS em metros :contentReference[oaicite:3]{index=3}
   
-  shape <- st_transform(shape, 31982)
-  parc_exist <- st_transform(parc_exist, 31982)
-  
+  # Transformação e marcação dos talhões
+  shape <- st_transform(shape, 31982)  
   shape$Index <- paste0(shape$ID_PROJETO, shape$TALHAO)
   parc_exist$Index <- paste0(parc_exist$PROJETO, parc_exist$TALHAO)
   
-  buffer_distance <- -15
+  # Buffer interno de -30 m
+  buffer_distance <- -30  # reduz polígonos em 30 m internamente :contentReference[oaicite:4]{index=4}
   
-  shapeb <- list()
-  empty_indexes <- c()
-  
-  for (i in 1:nrow(shape)) {
-    if (st_is_empty(st_buffer(shape[i,], buffer_distance))) {
-      empty_indexes <- c(empty_indexes, i)
-    } else {
-      dt_aux <- st_buffer(shape[i,], buffer_distance)
-      shapeb[[i]] <- dt_aux
-    }
-  }
-  
-  if(!is.null(empty_indexes)) {
-    shapeb <- shapeb[-empty_indexes]
-  }
-  shapeb <- do.call("rbind", shapeb)
+  # Aplica buffer e remove geometrias vazias
+  shapeb_list <- lapply(seq_len(nrow(shape)), function(i) {
+    buf <- st_buffer(shape[i, ], buffer_distance)
+    if (st_is_empty(buf)) NULL else buf
+  })
+  shapeb <- do.call(rbind, Filter(NROW, shapeb_list))
   
   result_points <- list()
-  points2 <- NULL
-  aux <- 1
-  tempo <- 0
+  total_poly <- length(unique(shapeb$Index))
+  completed   <- 0
   
-  total_poly_idx <- length(unique(shapeb$Index)) 
-  completed_poly_idx <- 0
-  
-  for (poly_idx in unique(shapeb$Index)) { 
-    
-    
-    poly <- shapeb[shapeb$Index == poly_idx,]
+  # Loop por cada talhão (Index)
+  for (poly_idx in unique(shapeb$Index)) {
+    poly <- shapeb[shapeb$Index == poly_idx, ]
     subgeoms <- split_subgeometries(poly)
     
-    for (i in 1:nrow(subgeoms)) {
-      if(as.numeric(st_area(subgeoms[i,])) < 400) {
-        next
+    for (j in seq_len(nrow(subgeoms))) {
+      sg <- subgeoms[j, ]
+      area_sg <- as.numeric(st_area(sg))
+      
+      # Pula áreas muito pequenas
+      if (area_sg < 400) next
+      
+      # Caso de área entre 400 e 1 000 m²: centróide
+      if (area_sg <= 1000) {
+        centroid <- st_centroid(st_geometry(sg))
+        pts_sf <- st_sf(
+          data.frame(Index      = poly_idx,
+                     PROJETO    = poly$ID_PROJETO,
+                     TALHAO     = poly$TALHAO,
+                     CICLO      = poly$CICLO,
+                     ROTACAO    = poly$ROTACAO,
+                     STATUS     = "ATIVA",
+                     FORMA      = forma_parcela,
+                     TIPO_INSTA = tipo_parcela,
+                     TIPO_ATUAL = tipo_parcela,
+                     DATA       = Sys.Date(),
+                     DATA_ATUAL = Sys.Date(),
+                     COORD_X    = st_coordinates(centroid)[,1],
+                     COORD_Y    = st_coordinates(centroid)[,2]),
+          geometry = centroid
+        )
+        
       } else {
-        sg <- subgeoms[i,]
-        sg_area <- as.numeric(st_area(sg))
-        active_points_all <- parc_exist[parc_exist$STATUS == "ATIVA" & parc_exist$Index == poly_idx,]
-        active_points <- st_intersection(st_geometry(active_points_all), st_geometry(sg))
+        # Caso de área > 1 000 m²: usa intensidade_amostral pontos em grade regular
+        n_pts <- intensidade_amostral  # número de pontos solicitado pelo usuário :contentReference[oaicite:5]{index=5}
         
-        if (sg_area >= 400 & sg_area <= 1000) {
-          if (length(active_points) > 0) {
-            next
-          } else {
-            cell.point <- st_centroid(st_geometry(sg))
-            
-            conf.point <- st_buffer(cell.point, dist = sqrt(400 / pi))
-            conf.point <- st_intersection(conf.point, sg) %>% st_sf()
-            
-            # if (as.numeric(st_area(conf.point)) < 400) {
-            #   next
-            # } else {
-              points2 <- st_sf(data.frame(Area = sg_area,
-                                          Index = poly_idx,
-                                          PROJETO = poly$ID_PROJETO,
-                                          TALHAO = poly$TALHAO,
-                                          CICLO = poly$CICLO,
-                                          ROTACAO = poly$ROTACAO,
-                                          STATUS = "ATIVA",
-                                          FORMA = forma_parcela, 
-                                          TIPO_INSTA = tipo_parcela,
-                                          TIPO_ATUAL = tipo_parcela, 
-                                          DATA = Sys.Date(),
-                                          DATA_ATUAL = Sys.Date(),
-                                          COORD_X = st_coordinates(cell.point)[1],
-                                          COORD_Y = st_coordinates(cell.point)[2]),
-                               geometry = st_geometry(cell.point))
-            # }
-          }
-        } else {
-          num_parc <- recomend[recomend$Index == poly_idx, "Num.parc"]
-          
-          if(nrow(subgeoms) > 1) { 
-            num_parc <- num_parc + nrow(subgeoms)
-            num_parc <- round(num_parc * (st_area(subgeoms) / st_area(poly)))
-            num_parc <- as.numeric(num_parc)
-            num_parc <- ifelse(num_parc < 1, 1, num_parc)
-            num_parc <- num_parc[i]
-            num_parc <- ifelse(num_parc >= recomend[recomend$Index == poly_idx, "Num.parc"],
-                               recomend[recomend$Index == poly_idx, "Num.parc"],
-                               num_parc)
-          } else {
-            num_parc <- num_parc
-          }
-          
-          d <- 2 * sqrt(400 / pi) 
-          grid <- st_make_grid(sg, cellsize = c(d, d), what = "polygons", square = T)
-          grid <- st_intersection(grid, sg) %>%  st_sf()
-          grid$area.grid <- round(as.numeric(st_area(grid)))
-          grid <- grid %>% filter(area.grid >= round(d^2))
-          
-          if(nrow(grid) == 0) {
-            next
-          }
-          
-          if(num_parc > nrow(grid)) {
-            num_parc <- nrow(grid)
-          }
-          
-          indices_grid <- sample(nrow(grid), num_parc)
-          grid_selecionado <- grid[indices_grid,]
-          
-          points_list <- list()
-          for (j in 1:nrow(grid_selecionado)) {
-            cell <- grid_selecionado[j,]
-            active_points_cell <- st_intersection(st_geometry(active_points), st_geometry(cell))
-            
-            if (length(active_points_cell) == 0) {
-              cell.point <- st_centroid(st_geometry(cell))
-              
-              area_vector <- as.numeric(st_area(cell))
-              index_vector <- rep(poly_idx, length(area_vector))
-              
-              points_list[[j]] <- st_sf(data.frame(Area = area_vector,
-                                                   Index = index_vector,
-                                                   PROJETO = poly$ID_PROJETO,
-                                                   TALHAO = poly$TALHAO,
-                                                   CICLO = poly$CICLO,
-                                                   ROTACAO = poly$ROTACAO,
-                                                   STATUS = "ATIVA",
-                                                   FORMA = forma_parcela, 
-                                                   TIPO_INSTA = tipo_parcela,
-                                                   TIPO_ATUAL = tipo_parcela, 
-                                                   DATA = Sys.Date(),
-                                                   DATA_ATUAL = Sys.Date(),
-                                                   COORD_X = st_coordinates(cell.point)[1],
-                                                   COORD_Y = st_coordinates(cell.point)[2]),
-                                        geometry = st_geometry(cell.point))
-            } else {
-              next
-            }
-          }
-          points2 <- do.call("rbind", points_list)
-        }
+        pts_sfc <- st_sample(x    = sg,
+                             size = n_pts,
+                             type = "regular")      # amostragem em grade regular :contentReference[oaicite:6]{index=6}
         
-        result_points[[paste(poly_idx, i, sep = "-" )]] <- points2
+        if (length(pts_sfc) == 0) next  # se não couber ponto algum, pula
+        
+        # Monta sf dos pontos com atributos
+        coords <- st_coordinates(pts_sfc)
+        pts_sf <- st_sf(
+          data.frame(Index      = rep(poly_idx, n_pts),
+                     PROJETO    = rep(poly$ID_PROJETO, n_pts),
+                     TALHAO     = rep(poly$TALHAO, n_pts),
+                     CICLO      = rep(poly$CICLO, n_pts),
+                     ROTACAO    = rep(poly$ROTACAO, n_pts),
+                     STATUS     = rep("ATIVA", n_pts),
+                     FORMA      = rep(forma_parcela, n_pts),
+                     TIPO_INSTA = rep(tipo_parcela, n_pts),
+                     TIPO_ATUAL = rep(tipo_parcela, n_pts),
+                     DATA       = rep(Sys.Date(), n_pts),
+                     DATA_ATUAL = rep(Sys.Date(), n_pts),
+                     COORD_X    = coords[,1],
+                     COORD_Y    = coords[,2]),
+          geometry = pts_sfc
+        )
       }
+      
+      result_points[[paste(poly_idx, j, sep = "_")]] <- pts_sf
     }
     
-    completed_poly_idx <- completed_poly_idx + 1
-    progress_percent <- round((completed_poly_idx / total_poly_idx) * 100,2)
-    update_progress(progress_percent)
+    # Atualiza barra de progresso
+    completed <- completed + 1
+    update_progress(round(completed / total_poly * 100, 2))
   }
   
-  
-  result_points <- do.call("rbind", result_points)
-  
+  # Combina todos os pontos e calcula numeração sequencial de parcelas
+  all_pts <- do.call(rbind, result_points)
   parcelasinv <- parc_exist %>%
     group_by(PROJETO) %>%
-    dplyr::summarise(numeracao = max(PARCELAS[PARCELAS < 500]),
-                     numeracao2 = max(PARCELAS)) %>% as.data.frame()
+    summarise(max_antiga = max(PARCELAS[PARCELAS < 500], na.rm = TRUE),
+              max_geral  = max(PARCELAS, na.rm = TRUE)) %>%
+    mutate(numeracao_inicial = if_else(tipo_parcela %in% c("IFQ6","IFQ12","S30","S90","PP"),
+                                       if_else(max_antiga == 499, max_geral + 1, max_antiga + 1),
+                                       if_else(max_antiga < 500, 501, max_antiga + 1))) %>%
+    select(PROJETO, numeracao_inicial)
   
-  if (tipo_parcela %in% c("IFQ6", "IFQ12", "S30", "S90", "PP")) {
-    parcelasinv <-  parcelasinv %>%
-      mutate(numeracao.inicial = if_else(numeracao == 499, numeracao2 + 1, numeracao + 1)) %>%
-      select(PROJETO, numeracao.inicial)
-  } else {
-    parcelasinv <- parcelasinv %>%
-      mutate(numeracao.inicial = replace(numeracao, numeracao < 500, 501)) %>%
-      select(PROJETO, numeracao.inicial)
-  }
-  
-  result_points <- result_points %>%
+  all_pts <- all_pts %>%
     left_join(parcelasinv, by = "PROJETO") %>%
-    mutate(numeracao.inicial = replace_na(numeracao.inicial, 1)) %>%
+    replace_na(list(numeracao_inicial = 1)) %>%
     group_by(PROJETO) %>%
-    mutate(PARCELAS = row_number() - 1 + first(numeracao.inicial)) %>%
+    mutate(PARCELAS = row_number() - 1 + first(numeracao_inicial)) %>%
     ungroup() %>%
-    select(-Area, -numeracao.inicial)
+    select(-numeracao_inicial)
   
-  return(result_points)
+  return(all_pts)
 }
