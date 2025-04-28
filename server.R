1201dd87fd0621eb6c96061d4bc4b657439a3efb
alek <alekviniciuscorreia@gmail.com>
2025-04-28 10:02:49 -0400

    Atualizar o server.R

--- server.R
+++ server.R
@@ -1,34 +1,16 @@
-output$download_result <- downloadHandler(
-  filename = function() {
-    data_str <- format(Sys.time(), "%d-%m-%y_%H.%M")
-    paste0("parcelas_", tipo_parcela(), "_", data_str, ".zip")  
-  },
-  content = function(file) {
-    req(values$result_points)
-    temp_dir <- tempdir()
-    shapefile_dir <- file.path(temp_dir, "parcelas")
-    dir.create(shapefile_dir, showWarnings = FALSE)
-    
-    # caminho completo do shapefile .shp
-    shapefile_path <- file.path(
-      shapefile_dir,
-      paste0("parcelas_", tipo_parcela(), "_", format(Sys.time(), "%d-%m-%y_%H.%M"), ".shp")
-    )
-    
-    # escreve todos os arquivos do shapefile
-    st_write(values$result_points, dsn = shapefile_path,
-             driver = "ESRI Shapefile", delete_dsn = TRUE)
-    
-    # lista *TODOS* os componentes do shapefile
-    shapefile_files <- list.files(
-      path = shapefile_dir,
-      pattern = "\\.(shp|shx|dbf|prj|cpg|qpj)$",
-      full.names = TRUE
-    )
-    
-    # comprime
-    zip::zipr(zipfile = file, files = shapefile_files,
-              root = shapefile_dir)
-  },
-  contentType = "application/zip"
-)
+content = function(file) {
+  req(values$result_points)
+  data_str <- format(Sys.time(), "%d-%m-%y_%H.%M")
+  # pasta única para este download
+  shapefile_dir <- tempfile(pattern = paste0("parcelas_", tipo_parcela(), "_", data_str, "_"))
+  dir.create(shapefile_dir)
+  
+  shp_path <- file.path(shapefile_dir,
+                        paste0("parcelas_", tipo_parcela(), "_", data_str, ".shp"))
+  st_write(values$result_points, dsn = shp_path, driver = "ESRI Shapefile", delete_dsn = TRUE)
+  
+  files <- list.files(shapefile_dir,
+                      pattern = "\\.(shp|shx|dbf|prj|cpg|qpj)$",
+                      full.names = TRUE)
+  zip::zipr(zipfile = file, files = files, root = shapefile_dir)
+}
