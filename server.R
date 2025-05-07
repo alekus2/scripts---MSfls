20fefa05ec5501a1fcb093a681d7327e949efb78
alek <alekviniciuscorreia@gmail.com>
2025-05-07 10:01:56 -0300

    Update server.R

--- server.R
+++ server.R
@@ -1,46 +1,19 @@
-shape <- reactive({
-  req(input$data_source)
-  shp <- if (input$data_source == "upload") {
-    req(input$shape)
-    zp      <- unzip(input$shape$datapath, exdir = tempdir())
-    shpfile <- grep("\\.shp$", zp, value = TRUE)
-    st_read(shpfile, quiet = TRUE)
-  } else {
-    req(session$userData$db_con, input$db_table)
-    dsn <- sprintf("OCI:%s/%s@%s:%s/%s",
-                   input$db_user, input$db_pwd,
-                   input$db_host, input$db_port,
-                   input$db_service)
-    st_read(dsn = dsn, layer = input$db_table, quiet = TRUE)
-  }
-
-  # valida se as colunas existem antes de renomear ou mutar
-  if (!("ID_PROJETO" %in% names(shp) &&
-        "TALHAO"     %in% names(shp))) {
-    validate(
-      need(
-        input$shape_input_pergunta_arudek == 0,
-        "Shape não tem colunas ID_PROJETO e TALHAO.  
-        Selecione ‘Outro’ e informe os nomes corretos antes de continuar."
-      )
-    )
-  }
-
-  if (isTRUE(input$shape_input_pergunta_arudek == 0)) {
-    shp <- shp %>%
-      rename(
-        ID_PROJETO = !!sym(input$mudar_nome_arudek_projeto),
-        TALHAO     = !!sym(input$mudar_nome_arudek_talhao),
-        CICLO      = !!sym(input$mudar_nome_arudek_ciclo),
-        ROTACAO    = !!sym(input$mudar_nome_arudek_rotacao)
-      )
-  }
-
-  shp %>%
-    st_transform(31982) %>%
-    mutate(
-      ID_PROJETO = str_pad(ID_PROJETO, 4, pad = "0"),
-      TALHAO     = str_pad(TALHAO,     3, pad = "0"),
-      Index      = paste0(ID_PROJETO, TALHAO)
-    )
-})
+Listening on http://127.0.0.1:3786
+Server iniciado
+renderUI: db_layer_selector
+observeEvent: confirmar
+observeEvent: gerar_parcelas iniciado
+parc_exist_path(): inicio
+parc_exist_path(): usando default data/parc.shp
+Reading layer `parc' from data source `F:\Qualidade_Florestal\02- MATO GROSSO DO SUL\11- Administrativo Qualidade MS\00- Colaboradores\17 - Alex Vinicius\AutomaÃ§Ã£o em R\AutoAlocador\data\parc.shp' using driver `ESRI Shapefile'
+Simple feature collection with 1 feature and 20 fields
+Geometry type: POINT
+Dimension:     XY
+Bounding box:  xmin: -49.21066 ymin: -22.63133 xmax: -49.21066 ymax: -22.63133
+Geodetic CRS:  SIRGAS 2000
+Aviso: Error in if: argumento tem comprimento zero
+  85: st_transform
+  84: mutate
+  81: observe [src/server.R#138]
+  80: <observer:observeEvent(input$gerar_parcelas)>
+   1: runApp
