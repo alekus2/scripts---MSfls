17bcd9379ee7696384fab17f365d5a62fe0bc78d
alek <alekviniciuscorreia@gmail.com>
2025-05-07 09:57:01 -0300

    Update server.R

--- server.R
+++ server.R
@@ -233,4 +233,25 @@ server <- function(input, output, session) {
   })
 }
 
-shinyApp(ui = ui, server = server)
+Listening on http://127.0.0.1:3786
+Server iniciado
+renderUI: db_layer_selector
+observeEvent: confirmar
+observeEvent: gerar_parcelas iniciado
+parc_exist_path(): inicio
+parc_exist_path(): usando default data/parc.shp
+Reading layer `parc' from data source `F:\Qualidade_Florestal\02- MATO GROSSO DO SUL\11- Administrativo Qualidade MS\00- Colaboradores\17 - Alex Vinicius\AutomaÃ§Ã£o em R\AutoAlocador\data\parc.shp' using driver `ESRI Shapefile'
+Simple feature collection with 1 feature and 20 fields
+Geometry type: POINT
+Dimension:     XY
+Bounding box:  xmin: -49.21066 ymin: -22.63133 xmax: -49.21066 ymax: -22.63133
+Geodetic CRS:  SIRGAS 2000
+shape(): inicio
+shape(): data_source =upload
+shape(): lendo shapefile do upload
+Aviso: Error in if: argumento tem comprimento zero
+  85: st_transform
+  84: mutate
+  81: observe [src/server.R#138]
+  80: <observer:observeEvent(input$gerar_parcelas)>
+   1: runApp
