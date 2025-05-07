6e9ad68fb499aabf48affb04c99679be2343eed0
alek <alekviniciuscorreia@gmail.com>
2025-05-07 10:12:40 -0300

    Update server.R

--- server.R
+++ server.R
@@ -196,4 +196,46 @@ server <- function(input, output, session) {
 
 }
 
-shinyApp(ui = ui, server = server)
+> runApp('F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/AutoAlocador/AutoParc.R')
+Aviso: Navigation containers expect a collection of `bslib::nav_panel()`/`shiny::tabPanel()`s and/or `bslib::nav_menu()`/`shiny::navbarMenu()`s. Consider using `header` or `footer` if you wish to place content above (or below) every panel's contents.
+
+Listening on http://127.0.0.1:3786
+Reading layer `parc' from data source `F:\Qualidade_Florestal\02- MATO GROSSO DO SUL\11- Administrativo Qualidade MS\00- Colaboradores\17 - Alex Vinicius\AutomaÃ§Ã£o em R\AutoAlocador\data\parc.shp' using driver `ESRI Shapefile'
+Simple feature collection with 1 feature and 20 fields
+Geometry type: POINT
+Dimension:     XY
+Bounding box:  xmin: -49.21066 ymin: -22.63133 xmax: -49.21066 ymax: -22.63133
+Geodetic CRS:  SIRGAS 2000
+Simple feature collection with 6 features and 66 fields
+Geometry type: MULTIPOLYGON
+Dimension:     XY
+Bounding box:  xmin: 256135.4 ymin: 7573138 xmax: 312547.7 ymax: 7670584
+Projected CRS: SIRGAS 2000 / UTM zone 22S
+  OBJECTID CD_USO_SOL ID_REGIAO       REGIAO ID_PROJETO                       PROJETO TALHAO TIPO_PROPR ID_GRUPO_U      GRUPO_USO_ ID_USO_SOL          USO_SOLO CICLO ROTACAO      REGIME  ESPACAMENT     GENERO                   ESPECIE MATERIAL_G
+1        1      48963         4 MS FLORESTAL       6455           BOM RETIRO II - RRP 002-01   Terceiro        100 Área cultivável        108 Plantio comercial     1       1 Implantação 3,60 x 2,50 Eucalyptus E. urophylla x E. pellita    AEC2475
+2        2      52725         4 MS FLORESTAL       6268               PONTAL II - SRP 027-01   Terceiro        100 Área cultivável        108 Plantio comercial     1       1 Implantação 3,60 x 2,50 Eucalyptus E. grandis x E. urophylla   SUZA0217
+3        3      49938         4 MS FLORESTAL       6436          JATOBÁ GLEBA A - NAD 066-01   Terceiro        100 Área cultivável        108 Plantio comercial     1       1 Implantação 3,60 x 2,50 Eucalyptus E. grandis x E. urophylla     CO1572
+4        4      47926         4 MS FLORESTAL       6443 NOSSA SENHORA DE FÁTIMA - ANR 006-01   Terceiro        100 Área cultivável        108 Plantio comercial     1       1 Implantação 3,60 x 2,50 Eucalyptus E. grandis x E. urophylla       IPB2
+5        5      47223         4 MS FLORESTAL       6443 NOSSA SENHORA DE FÁTIMA - ANR 010-01   Terceiro        100 Área cultivável        108 Plantio comercial     1       1 Implantação 3,60 x 2,50 Eucalyptus E. grandis x E. urophylla     CO1572
+6        6      47220         4 MS FLORESTAL       6443 NOSSA SENHORA DE FÁTIMA - ANR 007-01   Terceiro        100 Área cultivável        108 Plantio comercial     1       1 Implantação 3,60 x 2,50 Eucalyptus E. grandis x E. urophylla       IPB2
+       SIST_PROPA DATA_PLANT IDADE_PLAN AREA_HA DECLIVIDAD TIPO_SOLO BACIA_HIDR   BIOMA TIPOLOGIA           MUNICIPIO          ESTADO   PAIS CLASSE_SIT         UNIDADE_GE      CHAVE                      TIPO_CONTR PROJETO_EX DCAA_NUMER REGIONAL_C
+1 Clone eucalipto 2025-04-10       0.06   34.17       <NA>      <NA> Pardo (MS) Cerrado    Savana  Ribas do Rio Pardo MATO GROSSO SUL BRASIL       <NA> MS FLORESTAL (SIL) 6455002-01                  Parceria rural        MS3       <NA>       <NA>
+2 Clone eucalipto 2025-04-12       0.05    6.48       <NA>      <NA> Pardo (MS) Cerrado    Savana Santa Rita do Pardo MATO GROSSO SUL BRASIL       <NA> MS FLORESTAL (SIL) 6268027-01 Parceria rural (Nova Esperança)        MS3       <NA>       <NA>
+3 Clone eucalipto 2025-04-09       0.06    8.70       <NA>      <NA> Pardo (MS) Cerrado    Savana      Nova Andradina MATO GROSSO SUL BRASIL       <NA> MS FLORESTAL (SIL) 6436066-01                  Parceria rural        MS3       <NA>       <NA>
+4 Clone eucalipto 2025-03-31       0.08   77.15       <NA>      <NA>   Ivinhema Cerrado    Savana        Anaurilândia MATO GROSSO SUL BRASIL       <NA> MS FLORESTAL (SIL) 6443006-01                  Parceria rural        MS3       <NA>       <NA>
+5 Clone eucalipto 2025-04-11       0.05   56.37       <NA>      <NA>   Ivinhema Cerrado    Savana        Anaurilândia MATO GROSSO SUL BRASIL       <NA> MS FLORESTAL (SIL) 6443010-01                  Parceria rural        MS3       <NA>       <NA>
+6 Clone eucalipto 2025-04-02       0.08   99.44       <NA>      <NA>   Ivinhema Cerrado    Savana        Anaurilândia MATO GROSSO SUL BRASIL       <NA> MS FLORESTAL (SIL) 6443007-01                  Parceria rural        MS3       <NA>       <NA>
+  REGIONAL_S REGIÃO_CL CERTIFICAC TIPO_LICEN PRODUTO   COLETOR_CU DISTANCIA_ DISTANCIA1 DISTANCI_1 PRECIPITAC CICLO_INVE PORC_SAIDA DATA_PLA_1 DATA_INICI DATA_FIM_V DCAA_DATA_ DCAA_DATA1 DATA_PRIME CD_UNIDADE CD_REGIAO CD_PROJETO CD_TALHAO
+1       <NA>       CZ2       <NA>       <NA>    <NA> PP9564551125    28.6200     635.56     664.18          0          1          0 01/02/2025 01/11/2024 01/11/2039       <NA>       <NA>       <NA>          1        11       1595    002-01
+2       <NA>       CZ3       <NA>       <NA>    <NA> PP9562681125    27.9600     799.79     827.75          0          1          0 12/02/2025 12/11/2024       <NA>       <NA>       <NA>       <NA>          1        11       1434    027-01
+3       <NA>       CZ2       <NA>       <NA>    <NA> PP9564361125    16.1175     593.48     609.60          0          1          0 15/04/2025 01/09/2024 01/09/2039       <NA>       <NA>       <NA>          1        11       1576    066-01
+4       <NA>       CZ2       <NA>       <NA>    <NA> PP9564431125     0.0000     560.59     560.59          0          1          0 10/04/2025 10/01/2025 10/01/2040       <NA>       <NA>       <NA>          1        11       1583    006-01
+5       <NA>       CZ2       <NA>       <NA>    <NA> PP9564431125     0.0000     560.59     560.59          0          1          0 10/04/2025 10/01/2025 10/01/2040       <NA>       <NA>       <NA>          1        11       1583    010-01
+6       <NA>       CZ2       <NA>       <NA>    <NA> PP9564431125     0.0000     560.59     560.59          0          1          0 10/04/2025 10/01/2025 10/01/2040       <NA>       <NA>       <NA>          1        11       1583    007-01
+    DATA_REG BUFF_DIST ORIG_FID SHAPE_Leng   SHAPE_Area                       geometry      Index
+1 2025-03-25       -30   108293 0.02175040 2.342043e-05 MULTIPOLYGON (((256141.8 76... 6455002-01
+2 2025-03-25       -30   115351 0.01280840 1.761309e-06 MULTIPOLYGON (((293376.7 76... 6268027-01
+3 2025-03-25       -30   126487 0.01078748 4.128822e-06 MULTIPOLYGON (((278088.9 75... 6436066-01
+4 2025-03-25       -30   108758 0.03411427 5.754906e-05 MULTIPOLYGON (((311441.6 75... 6443006-01
+5 2025-03-25       -30   108887 0.02584657 4.166042e-05 MULTIPOLYGON (((312547.7 75... 6443010-01
+6 2025-03-25       -30   108916 0.03597395 7.659250e-05 MULTIPOLYGON (((311834.3 75... 6443007-01
