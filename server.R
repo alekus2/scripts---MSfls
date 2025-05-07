187ee5248cb7675f5acdfebdcc45b13f8110d3af
alek <alekviniciuscorreia@gmail.com>
2025-05-07 08:38:20 -0300

    Update server.R

--- server.R
+++ server.R
@@ -262,3 +262,19 @@ server <- function(input, output, session) {
       theme(plot.title = element_text(hjust = 0.5, face = "bold"))
   })
 }
+
+
+
+Listening on http://127.0.0.1:3786
+Reading layer `parc' from data source `F:\Qualidade_Florestal\02- MATO GROSSO DO SUL\11- Administrativo Qualidade MS\00- Colaboradores\17 - Alex Vinicius\AutomaÃ§Ã£o em R\AutoAlocador\data\parc.shp' using driver `ESRI Shapefile'
+Simple feature collection with 1 feature and 20 fields
+Geometry type: POINT
+Dimension:     XY
+Bounding box:  xmin: -49.21066 ymin: -22.63133 xmax: -49.21066 ymax: -22.63133
+Geodetic CRS:  SIRGAS 2000
+Aviso: Error in if: argumento tem comprimento zero
+  85: st_transform
+  84: mutate
+  81: observe [src/server.R#115]
+  80: <observer:observeEvent(input$gerar_parcelas)>
+   1: runApp
