a9a8a8d950e87f783d298cdaddf94581101daf81
alek <alekviniciuscorreia@gmail.com>
2025-05-09 08:54:47 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,3 +1,4 @@
+
 library(shiny)
 library(shinythemes)
 
@@ -65,14 +66,6 @@ ui <- fluidPage(
         max-height: 40px;
         margin-right: 10px;
       }
-      /* Estilo personalizado para a sidebar */
-      .custom-sidebar {
-        width: 300px; /* Defina a largura desejada */
-        float: left; /* Mantenha a sidebar flutuando à esquerda */
-      }
-      .main-panel {
-        margin-left: 320px; /* Espaco para a largura da sidebar */
-      }
     ")))
   ),
   
@@ -81,63 +74,56 @@ ui <- fluidPage(
     
     tabPanel("Dados", icon = icon("file-upload"),
              sidebarLayout(
-               sidebarPanel(class = "custom-sidebar",
-                 radioButtons("data_source", "Fonte dos talhões:",
-                              choices = c("Upload shapefile (.zip)" = "upload"),
-                              selected = "upload"),
-                 
-                 conditionalPanel(
-                   "input.data_source == 'upload'",
-                   fileInput("shape", "Upload do Shapefile dos talhões (.zip)", accept = c(".zip"))
-                 ),
-                 radioButtons("shape_input_pergunta_arudek", label = h3("Qual o formato do shape de entrada?"),
-                              choices = list("P_SDE_BRACELL_PUB.VW_GIS_POL_USO_SOLO_MS" = 1, "Outro" = 0), selected = 1),
-                 conditionalPanel("input.shape_input_pergunta_arudek == 0",
-                                  textInput("mudar_nome_arudek_projeto", "Projeto:", "ID_PROJETO"),
-                                  textInput("mudar_nome_arudek_talhao",   "Talhão:",  "CD_TALHAO"),
-                                  textInput("mudar_nome_arudek_ciclo",    "Ciclo:",   "CICLO"),
-                                  textInput("mudar_nome_arudek_rotacao",  "Rotação:", "ROTACAO")
-                 ),
-                 numericInput("intensidade_amostral", "Intensidade amostral (parcelas/ha):", value = 5),
-                 radioButtons("parcelas_existentes_lancar", "Deseja informar parcelas existentes?",
-                              choices = list("Sim" = 1, "Não" = 0), selected = 0),
-                 conditionalPanel(
-                   "input.parcelas_existentes_lancar == 1",
-                   fileInput("parc_exist", "Upload do Shapefile das parcelas existentes", accept = c(".zip"))
-                 ),
-                 selectizeInput("forma_parcela", "Forma da parcela:", choices = c("CIRCULAR", "RETANGULAR")),
-                 selectizeInput("tipo_parcela",  "Tipo da parcela:",   choices = c("S30", "S90", "IFQ6", "IFQ12", "IFC", "IPC")),
-                 conditionalPanel(
-                   "input.tipo_parcela == 'IPC'",
-                   radioButtons("lancar_sobrevivencia", "Lançar parcelas de sobrevivência?",
-                                choices = list("Sim" = 1, "Não" = 0), selected = 0)
-                 ),
-                 sliderInput("distancia_minima", "Distância mínima (m):", min = 30, max = 100, value = 50, step = 10),
-                 actionButton("confirmar", "Confirmar", class = "btn btn-danger")
+               sidebarPanel(radioButtons("data_source", "Fonte dos talhões:",
+                                         choices = c("Upload shapefile (.zip)" = "upload"),
+                                         selected = "upload"),
+                            
+                            conditionalPanel(
+                              "input.data_source == 'upload'",
+                              fileInput("shape", "Upload do Shapefile dos talhões (.zip)", accept = c(".zip"))
+                            ),
+                            radioButtons("shape_input_pergunta_arudek", label = h3("Qual o formato do shape de entrada?"),
+                                         choices = list("P_SDE_BRACELL_PUB...ETC" = 1, "Outro" = 0), selected = 1),
+                            conditionalPanel("input.shape_input_pergunta_arudek == 0",
+                                             textInput("mudar_nome_arudek_projeto", "Projeto:", "ID_PROJETO"),
+                                             textInput("mudar_nome_arudek_talhao",   "Talhão:",  "CD_TALHAO"),
+                                             textInput("mudar_nome_arudek_ciclo",    "Ciclo:",   "CICLO"),
+                                             textInput("mudar_nome_arudek_rotacao",  "Rotação:", "ROTACAO")
+                            ),
+                            numericInput("intensidade_amostral", "Intensidade amostral (parcelas/ha):", value = 5),
+                            selectizeInput("forma_parcela", "Forma da parcela:", choices = c("CIRCULAR", "RETANGULAR")),
+                            selectizeInput("tipo_parcela",  "Tipo da parcela:",   choices = c("S30", "S90", "IFQ6", "IFQ12", "IFC", "IPC")),
+                            conditionalPanel(
+                              "input.tipo_parcela == 'IPC'",
+                              radioButtons("lancar_sobrevivencia", "Lançar parcelas de sobrevivência?",
+                                           choices = list("Sim" = 1, "Não" = 0), selected = 0)
+                            ),
+                            sliderInput("distancia_minima", "Distância mínima (m):", min = 30, max = 100, value = 50, step = 10),
+                            actionButton("confirmar", "Confirmar", class = "btn btn-danger")
                ),
-               mainPanel(class = "main-panel", # Aplica a classe ao mainPanel
-                 div(class = "sobre-texto",
-                     h2("Sobre os arquivos"),
-                     br(),
-                     div(tags$b("Shape dos talhões:"), " .zip contendo os arquivos do shapefile."),
-                     div(tags$b("Banco de dados:"),  " banco de dados com suas informações de login e senha."),
-                     div(tags$b("Parcelas existentes:"), " .zip com o shapefile das parcelas existentes."),
-                     br(),
-                     p(style="color:red;
+               mainPanel( 
+                         div(class = "sobre-texto",
+                             h2("Sobre os arquivos"),
+                             br(),
+                             div(tags$b("Shape dos talhões:"), " .zip contendo os arquivos do shapefile."),
+
+                             div(tags$b("Parcelas existentes:"), " .zip com o shapefile das parcelas existentes."),
+                             br(),
+                             p(style="color:red;
                               font-size:16px;
                               font-weight:bold",
-                    "Lembre sempre de conferir a distancia entre as parcelas, a Distância Miníma pode alterar a quantidade de parcelas a serem alocadas!")
-                 ), 
-                 br(),
-                 div(class="sobre-texto",
-                     h2("Como plotar?"),
-                     br(),
-                     p(style = "color:black;font-weight:semi-bold",
-                     "Altere os dados que deseja, clique em 'Confirmar'. Em seguida vá na aba 'Plotagem' e clique em 'Gerar Parcelas'.")
-                     ),
-                 verbatimTextOutput("shape_text"),
-                 verbatimTextOutput("parc_exist_text"),
-                 verbatimTextOutput("confirmation")
+                               "Lembre sempre de conferir a distancia entre as parcelas, a Distância Miníma pode alterar a quantidade de parcelas a serem alocadas!")
+                         ), 
+                         br(),
+                         div(class="sobre-texto",
+                             h2("Como plotar?"),
+                             br(),
+                             p(style = "color:black;font-weight:semi-bold",
+                               "Altere os dados que deseja, clique em 'Confirmar'. Em seguida vá na aba 'Plotagem' e clique em 'Gerar Parcelas'.")
+                         ),
+                         verbatimTextOutput("shape_text"),
+                         verbatimTextOutput("parc_exist_text"),
+                         verbatimTextOutput("confirmation")
                )
              )
     ),
@@ -151,10 +137,10 @@ ui <- fluidPage(
                             actionButton("gerar_parcelas", "Gerar Parcelas", class = "btn btn-danger")
                           ),
                           mainPanel(class = "main-panel",
-                            div(id = "progress-container", style = "width:100%; background-color:#f3f3f3; padding:3px;",
-                                div(id = "progress-bar", style = "width:0%; height:20px; text-align:center; line-height:20px; color:white;")
-                            ),
-                            div(id = "completed-message", style = "display:none; font-weight:bold; color:green;", "Concluído")
+                                    div(id = "progress-container", style = "width:100%; background-color:#f3f3f3; padding:3px;",
+                                        div(id = "progress-bar", style = "width:0%; height:20px; text-align:center; line-height:20px; color:white;")
+                                    ),
+                                    div(id = "completed-message", style = "display:none; font-weight:bold; color:green;", "Concluído")
                           )
                         )
                ),
@@ -226,3 +212,6 @@ ui <- fluidPage(
 )
 
 shinyApp(ui = ui, server = function(input, output) { })
+
+
+Aviso: Navigation containers expect a collection of `bslib::nav_panel()`/`shiny::tabPanel()`s and/or `bslib::nav_menu()`/`shiny::navbarMenu()`s. Consider using `header` or `footer` if you wish to place content above (or below) every panel's contents.
