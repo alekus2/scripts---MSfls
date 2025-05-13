27fc1ea301331b36d0d8cbc79a6355a0b2eeaa63
alek <alekviniciuscorreia@gmail.com>
2025-05-13 10:11:58 -0400

    Atualizar o OtimizadorIFQ6 - Python.py

--- OtimizadorIFQ6 - Python.py
+++ OtimizadorIFQ6 - Python.py
@@ -1,4 +1,12 @@
-df_final = df_final.sort_values(by=['CD_PROJETO','CD_TALHAO','NM_PARCELA','NM_ALTURA'])
-df_final['nm_cova_ordenado'] = df_final.groupby(['CD_PROJETO','CD_TALHAO','NM_PARCELA']).cumcount() + 1
-df_final = df_final.sort_values(by=['CD_PROJETO','CD_TALHAO','NM_PARCELA','nm_cova_ordenado'])
-df_final.drop(columns=['check dup','check cd','check SQC'], inplace=True)
+df_final['equipe_2'] = df_final['EQUIPE']
+df_final['Dt_medição'] = df_final['DT_INICIAL']
+df_final['chave_2'] = (
+    df_final['CD_PROJETO'].astype(str) + '-' +
+    df_final['CD_TALHAO'].astype(str) + '-' +
+    df_final['NM_PARCELA'].astype(str)
+)
+df_final['Ht_média'] = df_final['ht média'].apply(lambda x: f"{x:.1f}".replace('.',','))
+df_final = df_final.sort_values(
+    by=['CD_PROJETO','CD_TALHAO','NM_PARCELA','nm_cova_ordenado']
+)
+df_final = df_final[['equipe_2','Dt_medição','chave_2','nm_cova_ordenado','Ht_média']]
