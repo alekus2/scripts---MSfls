cbc028a24c46a05d08206014716606c689b0835e
alek <alekviniciuscorreia@gmail.com>
2025-05-19 11:07:16 -0400

    Atualizar o OtimizadorIFQ6 - Python.py

--- OtimizadorIFQ6 - Python.py
+++ OtimizadorIFQ6 - Python.py
@@ -1,20 +1,9 @@
-# Cálculo bruto
-pits_por_sob_real = df_tabela["Stand (tree/ha)"] / pct
+def ajustar_pits_por_sob(row):
+    original = row["Stand (tree/ha)"] / (float(row["%_Sobrevivência"].replace(",", ".").replace("%", "")) / 100)
+    check_diff = original - row["Pits/ha"]
+    if abs(round(check_diff)) == 1:
+        return original  # mantém valor original (com casas decimais)
+    else:
+        return math.floor(original)  # arredonda para baixo
 
-# Cálculo arredondado padrão
-pits_por_sob_ceiled = pits_por_sob_real.apply(math.ceil)
-
-# Calcular Check pits inicialmente com valor ceiled
-check_pits_inicial = pits_por_sob_ceiled - df_tabela["Pits/ha"]
-
-# Aplicar a lógica condicional
-df_tabela["Pits por sob"] = np.where(
-    check_pits_inicial == 1, pits_por_sob_real,  # não arredonda
-    np.where(
-        check_pits_inicial == -1, pits_por_sob_ceiled,  # arredonda
-        pits_por_sob_ceiled  # padrão
-    )
-)
-
-# Recalcular o Check pits com base no valor final
-df_tabela["Check pits"] = df_tabela["Pits por sob"] - df_tabela["Pits/ha"]
+df_tabela["Pits por sob"] = df_tabela.apply(ajustar_pits_por_sob, axis=1)
