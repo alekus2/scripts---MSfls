df151b7f933c855992f84baec8a3c5d544ddbfc4
alek <alekviniciuscorreia@gmail.com>
2025-05-20 09:54:39 -0300

    Update OtimizadorIFQ6 - Python.py

--- OtimizadorIFQ6 - Python.py
+++ OtimizadorIFQ6 - Python.py
@@ -1,14 +1,16 @@
-df_D_tabela = df_tabela.copy()
-cols_cova_ordenado = [col for col in df_D_tabela.columns if col.isdigit()]
-for col in cols_cova_ordenado:
-    df_D_tabela[col] = df_D_tabela[col] ** 3
+---------------------------------------------------------------------------
+AttributeError                            Traceback (most recent call last)
+<ipython-input-6-ada6743c24ad> in <cell line: 0>()
+    326     "/content/Cadastro SGF (correto).xlsx"
+    327 ]
+--> 328 otimizador.validacao(arquivos)
 
-metrics_D = df_D_tabela.apply(_calc_row, axis=1)
-df_D_tabela = pd.concat([df_D_tabela, metrics_D], axis=1)
+1 frames
+/usr/local/lib/python3.11/dist-packages/pandas/core/generic.py in __getattr__(self, name)
+   6297         ):
+   6298             return self[name]
+-> 6299         return object.__getattribute__(self, name)
+   6300 
+   6301     @final
 
-if "PV50" in df_D_tabela.columns:
-    df_D_tabela["PV50"] = df_D_tabela["PV50"].astype(str).str.replace(",", ".").str.replace("%", "")
-    df_D_tabela["PV50"] = pd.to_numeric(df_D_tabela["PV50"], errors='coerce')
-    df_D_tabela["PV50"] = df_D_tabela["PV50"].map(lambda x: f"{x:.2f}%".replace(".", ",") if pd.notnull(x) else "0,00%")
-else:
-    print("A coluna PV50 não foi encontrada em D_Tabela.")
+AttributeError: 'DataFrame' object has no attribute 'str'
