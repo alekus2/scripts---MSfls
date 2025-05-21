e352709190a1d21ad02c968ae8479ffaca5025ad
alek <alekviniciuscorreia@gmail.com>
2025-05-21 11:26:32 -0300

    Update process_data.R

--- process_data.R
+++ process_data.R
@@ -1,12 +1,112 @@
- runApp('F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/AutoAlocador/AutoAlocar.R')
+library(glue)
+library(sf)
+library(dplyr)
 
-Listening on http://127.0.0.1:5628
-Reading layer `parc' from data source `F:\Qualidade_Florestal\02- MATO GROSSO DO SUL\11- Administrativo Qualidade MS\00- Colaboradores\17 - Alex Vinicius\AutomaÃ§Ã£o em R\AutoAlocador\data\parc.shp' using driver `ESRI Shapefile'
-Simple feature collection with 1 feature and 20 fields
-Geometry type: POINT
-Dimension:     XY
-Bounding box:  xmin: -49.21066 ymin: -22.63133 xmax: -49.21066 ymax: -22.63133
-Geodetic CRS:  SIRGAS 2000
+process_data <- function(shape, parc_exist_path,
+                         forma_parcela, tipo_parcela,
+                         distancia.minima,
+                         distancia_parcelas,
+                         intensidade_amostral,
+                         update_progress) {
+  
+  parc_exist <- suppressMessages(st_read(parc_exist_path)) %>% 
+    st_transform(31982)
+  
+  shape_full <- shape %>%
+    st_transform(31982) %>%
+    mutate(
+      Index = paste0(ID_PROJETO, TALHAO),
+      AREA_HA = if ("AREA_HA" %in% names(.)) 
+        as.numeric(AREA_HA)
+      else as.numeric(st_area(.) / 10000)
+    )
+  
+  shapeb <- shape_full %>%
+    st_buffer(-abs(distancia.minima)) %>%
+    filter(!st_is_empty(geometry) & st_is_valid(geometry))
+  
+  indices <- unique(shapeb$Index)
+  total_poly <- length(indices)
+  result_pts <- vector("list", total_poly)
+  
+  for (i in seq_along(indices)) {
+    idx <- indices[i]
+    talhao <- shapeb[shapeb$Index == idx, ]
+    if (nrow(talhao) == 0) next
+    if (any(st_is_empty(talhao)) || any(!st_is_valid(talhao))) next
+    index <- talhao$Index
+    area_ha <- talhao$AREA_HA[1]
+    n_req <- max(2, ceiling(area_ha / intensidade_amostral))
+    
+    delta <- sqrt(as.numeric(st_area(talhao)) / n_req)
+    
+    print(glue("Talhão: {index} | Número de parcelas recomendadas: {n_req} | Distância inicial entre parcelas (delta): {round(delta, 2)} m | Intensidade amostral: {intensidade_amostral} | Área do talhão: {round(area_ha, 2)} ha"))
+    
+    max_iter <- 50
+    iter <- 0
+    pts_sel <- NULL
+    
+    while (iter < max_iter) {
+      bb <- st_bbox(talhao)
+      offset <- c(bb$xmin + delta / 2, bb$ymin + delta / 2)
+      
+      grid_all <- st_make_grid(talhao, cellsize = c(delta, delta), offset = offset, what = "centers")
+      grid_all <- st_cast(grid_all, "POINT")
+      inside_poly <- st_within(grid_all, talhao, sparse = FALSE)[,1]
+      pts_tmp <- grid_all[inside_poly]
+      
+      print(glue("Iteração {iter + 1}: {length(pts_tmp)} pontos encontrados | delta = {round(delta, 2)}"))
+      
+      if (length(pts_tmp) == n_req) {
+        pts_sel <- pts_tmp
+        break
+      } else if (length(pts_tmp) < n_req) {
+        delta <- delta * 0.9
+      } else {
+        delta <- delta * 1.1
+      }
+      
+      iter <- iter + 1
+    }
+    
+    if (is.null(pts_sel) || length(pts_sel) != n_req) {
+      next
+    }
+    
+    cr <- st_coordinates(pts_sel)
+    ord <- order(cr[,1], cr[,2])
+    sel <- pts_sel[ord][seq_len(n_req)]
+    
+    df <- tibble(
+      Index      = idx,
+      PROJETO    = talhao$ID_PROJETO[1],
+      TALHAO     = talhao$TALHAO[1],
+      CICLO      = talhao$CICLO[1],
+      ROTACAO    = talhao$ROTACAO[1],
+      STATUS     = "ATIVA",
+      FORMA      = forma_parcela,
+      TIPO_INSTA = tipo_parcela,
+      TIPO_ATUAL = tipo_parcela,
+      DATA       = Sys.Date(),
+      DATA_ATUAL = Sys.Date(),
+      COORD_X    = st_coordinates(sel)[,1],
+      COORD_Y    = st_coordinates(sel)[,2],
+      AREA_HA    = area_ha
+    )
+    
+    pts_sf <- st_sf(df, geometry = sel, crs = st_crs(shape_full))
+    result_pts[[i]] <- pts_sf
+    update_progress(round(i/total_poly * 100, 1))
+  }
+  
+  all_pts <- bind_rows(result_pts)
+  
+  all_pts %>%
+    group_by(Index) %>%
+    mutate(PARCELA = row_number()) %>%
+    ungroup()
+}
+
 Talhão: 6455002-01 | Número de parcelas recomendadas: 7 | Distância inicial entre parcelas (delta): 170.59 m | Intensidade amostral: 5 | Área do talhão: 34.17 ha
 Iteração 1: 7 pontos encontrados | delta = 170.59
 Talhão: 6436066-01 | Número de parcelas recomendadas: 2 | Distância inicial entre parcelas (delta): 94.86 m | Intensidade amostral: 5 | Área do talhão: 8.7 ha
