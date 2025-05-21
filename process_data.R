08167707d97a198ac1d57118f75eed46b2a801d8
alek <alekviniciuscorreia@gmail.com>
2025-05-21 08:59:38 -0400

    Atualizar o process_data.R

--- process_data.R
+++ process_data.R
@@ -1,110 +1,103 @@
 library(sf)
 library(dplyr)
-library(glue)
 
 process_data <- function(shape, parc_exist_path,
                          forma_parcela, tipo_parcela,
                          distancia.minima,
                          distancia_parcelas,
-                         forma_parcela,
-                         intensidade_amostral,  
+                         intensidade_amostral,
                          update_progress) {
-
-  parc_exist <- st_read(parc_exist_path) %>% st_transform(31982)
+  
+  parc_exist <- suppressMessages(st_read(parc_exist_path)) %>% 
+    st_transform(31982)
+  
   shape_full <- shape %>%
     st_transform(31982) %>%
-    mutate(Index = paste0(ID_PROJETO, TALHAO),
-           AREA_HA = as.numeric(AREA_HA))
-
+    mutate(
+      Index = paste0(ID_PROJETO, TALHAO),
+      AREA_HA = if ("AREA_HA" %in% names(.)) 
+        as.numeric(AREA_HA) 
+      else as.numeric(st_area(.) / 10000)
+    )
+  
   shapeb <- shape_full %>%
     st_buffer(-abs(distancia.minima)) %>%
-    filter(!st_is_empty(geometry))
-
-  result_points <- list()
+    filter(!st_is_empty(geometry) & st_is_valid(geometry))
+  
   indices <- unique(shapeb$Index)
   total_poly <- length(indices)
-
+  result_pts <- vector("list", total_poly)
+  
   for (i in seq_along(indices)) {
     idx <- indices[i]
-    talhao <- filter(shapeb, Index == idx)
-    area_ha <- unique(talhao$AREA_HA)
+    talhao <- shapeb[shapeb$Index == idx, ]
+    if (nrow(talhao) == 0) next
+    if (any(st_is_empty(talhao)) || any(!st_is_valid(talhao))) next
+    
+    area_ha <- talhao$AREA_HA[1]
     n_req <- max(2, ceiling(area_ha / intensidade_amostral))
-    delta_base <- sqrt(as.numeric(st_area(talhao)) / n_req)
-    delta <- delta_base
-    bb <- st_bbox(talhao)
-    offset_xy <- c(bb$xmin + delta/2, bb$ymin + delta/2)
-
-    found <- FALSE
-    for (iter in seq_len(100)) {
-      grid_pts <- st_make_grid(talhao, cellsize = c(delta, delta), offset = offset_xy, what = "centers")
-      inside <- st_within(grid_pts, talhao, sparse = FALSE)
-      pts_tmp <- grid_pts[apply(inside, 1, any)]
+    
+    delta <- sqrt(as.numeric(st_area(talhao)) / n_req)
+    delta_step <- 1
+    max_iter <- 50
+    iter <- 0
+    pts_sel <- NULL
+    
+    while (iter < max_iter) {
+      bb <- st_bbox(talhao)
+      offset <- c(bb$xmin + delta / 2, bb$ymin + delta / 2)
+      
+      grid_all <- st_make_grid(talhao, cellsize = c(delta, delta), offset = offset, what = "centers")
+      grid_all <- st_cast(grid_all, "POINT")
+      inside_poly <- st_within(grid_all, talhao, sparse = FALSE)[,1]
+      pts_tmp <- grid_all[inside_poly]
+      
       if (length(pts_tmp) == n_req) {
-        pts_all <- pts_tmp
-        found <- TRUE
+        pts_sel <- pts_tmp
         break
-      } else if (length(pts_tmp) > n_req) {
-        delta <- delta + 1
+      } else if (length(pts_tmp) < n_req) {
+        delta <- delta * 0.9
       } else {
-        break
+        delta <- delta * 1.1
       }
+      
+      iter <- iter + 1
     }
-
-    if (!found) {
-      delta <- delta - 1
-      for (iter in seq_len(100)) {
-        grid_pts <- st_make_grid(talhao, cellsize = c(delta, delta), offset = offset_xy, what = "centers")
-        inside <- st_within(grid_pts, talhao, sparse = FALSE)
-        pts_tmp <- grid_pts[apply(inside, 1, any)]
-        if (length(pts_tmp) >= n_req) {
-          pts_all <- pts_tmp
-          break
-        }
-        delta <- delta - 1
-        if (delta <= 1) break
-      }
+    
+    if (is.null(pts_sel) || length(pts_sel) != n_req) {
+      next
     }
-
-    if (!exists("pts_all")) {
-      pts_all <- st_centroid(talhao)
-      while (length(pts_all) < n_req) pts_all <- c(pts_all, pts_all[1])
-    }
-
-    cr <- st_coordinates(pts_all)
+    
+    cr <- st_coordinates(pts_sel)
     ord <- order(cr[,1], cr[,2])
-    sel <- pts_all[ord][1:n_req]
-
-    coords <- st_coordinates(sel)
-    pts_sf <- st_sf(
-      data.frame(
-        Index      = rep(idx, n_req),
-        PROJETO    = rep(talhao$ID_PROJETO[1], n_req),
-        TALHAO     = rep(talhao$TALHAO[1],    n_req),
-        CICLO      = rep(talhao$CICLO[1],     n_req),
-        ROTACAO    = rep(talhao$ROTACAO[1],   n_req),
-        STATUS     = rep("ATIVA",             n_req),
-        FORMA      = rep(forma_parcela,       n_req),
-        TIPO_INSTA = rep(tipo_parcela,        n_req),
-        TIPO_ATUAL = rep(tipo_parcela,        n_req),
-        DATA       = rep(Sys.Date(),          n_req),
-        DATA_ATUAL = rep(Sys.Date(),          n_req),
-        COORD_X    = coords[,1],
-        COORD_Y    = coords[,2],
-        AREA_HA    = rep(area_ha, n_req)
-      ),
-      geometry = sel
+    sel <- pts_sel[ord][seq_len(n_req)]
+    
+    df <- tibble(
+      Index      = idx,
+      PROJETO    = talhao$ID_PROJETO[1],
+      TALHAO     = talhao$TALHAO[1],
+      CICLO      = talhao$CICLO[1],
+      ROTACAO    = talhao$ROTACAO[1],
+      STATUS     = "ATIVA",
+      FORMA      = forma_parcela,
+      TIPO_INSTA = tipo_parcela,
+      TIPO_ATUAL = tipo_parcela,
+      DATA       = Sys.Date(),
+      DATA_ATUAL = Sys.Date(),
+      COORD_X    = st_coordinates(sel)[,1],
+      COORD_Y    = st_coordinates(sel)[,2],
+      AREA_HA    = area_ha
     )
-
-    result_points[[idx]] <- pts_sf
-    update_progress(round(i / total_poly * 100, 1))
+    
+    pts_sf <- st_sf(df, geometry = sel, crs = st_crs(shape_full))
+    result_pts[[i]] <- pts_sf
+    update_progress(round(i/total_poly * 100, 1))
   }
-
-  all_pts <- do.call(rbind, result_points)
-
-  all_pts <- all_pts %>%
+  
+  all_pts <- bind_rows(result_pts)
+  
+  all_pts %>%
     group_by(Index) %>%
-    mutate(PARCELA = row_number()) %>% 
+    mutate(PARCELA = row_number()) %>%
     ungroup()
-
-  all_pts
 }
