ef6f6c0a5b8bf060384fe4d96a463c1547285681
alek <alekviniciuscorreia@gmail.com>
2025-06-02 08:26:59 -0400

    Update OtimizadorIFQ6.R

--- OtimizadorIFQ6.R
+++ OtimizadorIFQ6.R
@@ -18,11 +18,11 @@ validacao_ifq6 <- function(paths) {
   )
   cadastro_path <- paths[str_detect(toupper(basename(paths)), "SGF")][1]
   arquivos_ifq6 <- paths[!paths %in% cadastro_path & file.exists(paths)]
-  
+  #ele deveria ler os asrquivos mas o cadastro n entra nisso de separar e talz ele so serve para as outras modificações
   ler_ifq6 <- function(path) {
-    df <- tryCatch(read_excel(path, sheet = 1), error = function(e) NULL)
+    df <- tryCatch(read_excel(path, sheet = 0), error = function(e) NULL)
     if (is.null(df) || nrow(df) == 0) {
-      df <- tryCatch(read_excel(path, sheet = 2), error = function(e) NULL)
+      df <- tryCatch(read_excel(path, sheet = 1), error = function(e) NULL)
     }
     if (is.null(df) || nrow(df) == 0) return(NULL)
     names(df) <- ifelse(names(df) == "", paste0("new_col_", seq_along(df)), names(df))
@@ -205,7 +205,7 @@ validacao_ifq6 <- function(paths) {
   message("Tudo gravado em '", out, "'")
 }
 
-pasta_dados <- "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados"
+pasta_dados <- "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at"
 todos_xlsx <- list.files(
   path        = pasta_dados,
   pattern     = "\\.xlsx$",
@@ -221,3 +221,73 @@ stopifnot(all(file.exists(arquivos)))
 print(arquivos)
 
 validacao_ifq6(arquivos)
+
+
+> source("F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/OtimizadorIFQ6.R", encoding = 'UTF-8', echo=TRUE)
+
+> library(readxl)
+
+> library(dplyr)
+
+> library(tidyr)
+
+> library(purrr)
+
+> library(stringr)
+
+> library(lubridate)
+
+> library(openxlsx)
+
+> library(glue)
+
+> library(scales)
+
+> validacao_ifq6 <- function(paths) {
++   cols_esperadas <- c(
++     "CD_PROJETO","CD_TALHAO","NM_PARCELA","DC_TIPO_PARCELA","NM_AREA_PARCELA",
++      .... [TRUNCATED] 
+
+> pasta_dados <- "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/Ot ..." ... [TRUNCATED] 
+
+> todos_xlsx <- list.files(
++   path        = pasta_dados,
++   pattern     = "\\.xlsx$",
++   full.names  = TRUE
++ )
+
+> cadastro <- todos_xlsx[grepl("SGF", toupper(basename(todos_xlsx)))]
+
+> ifq6      <- setdiff(todos_xlsx, cadastro)
+
+> arquivos <- c(
++   cadastro,
++   ifq6
++ )
+
+> stopifnot(all(file.exists(arquivos)))  
+
+> print(arquivos)
+ [1] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/Cadastro SGF (correto).xlsx"                  
+ [2] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/6271_TABOCA_SRP - IFQ6 (4).xlsx"              
+ [3] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/6304_DOURADINHA_I_GLEBA_A_RRP - IFQ6 (8).xlsx"
+ [4] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/6348_BERRANTE_II_RRP - IFQ6 (29).xlsx"        
+ [5] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/6362_PONTAL_III_GLEBA_A_RRP - IFQ6 (22).xlsx" 
+ [6] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/6371_SÃO_ROQUE_BTG - IFQ6 (33).xlsx"          
+ [7] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/6371_SÃO_ROQUE_BTG - IFQ6 (8).xlsx"           
+ [8] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/6418_SÃO_JOÃO_IV_SRP - IFQ6 (6) - Copia.xlsx" 
+ [9] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/6418_SÃO_JOÃO_IV_SRP - IFQ6 (6).xlsx"         
+[10] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/6439_TREZE_DE_JULHO_RRP - IFQ6 (4).xlsx"      
+[11] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/base_dados_IFQ6_propria_fev.xlsx"             
+[12] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/IFQ6_MS_Florestal_Bravore_10032025.xlsx"      
+[13] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/IFQ6_MS_Florestal_Bravore_17032025.xlsx"      
+[14] "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/dados at/IFQ6_MS_Florestal_Bravore_24032025.xlsx"      
+
+> validacao_ifq6(arquivos)
+Error in `map()`:
+i In index: 1.
+Caused by error in `mutate()`:
+i In argument: `CD_PROJETO = as.character(CD_PROJETO)`.
+Caused by error:
+! objeto 'CD_PROJETO' não encontrado
+Run `rlang::last_trace()` to see where the error occurred.
