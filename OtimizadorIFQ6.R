a8c20e3d773ebc08b0ca662b6037b8493964c85f
alek <alekviniciuscorreia@gmail.com>
2025-06-02 09:48:42 -0400

    Update OtimizadorIFQ6.R

--- OtimizadorIFQ6.R
+++ OtimizadorIFQ6.R
@@ -1,15 +1,46 @@
-nome_arquivo <- basename(p)
-if (str_detect(nome_arquivo, regex("lebatec", ignore_case = TRUE))) {
-  base <- "lebatec"
-} else if (str_detect(nome_arquivo, regex("bravore", ignore_case = TRUE))) {
-  base <- "bravore"
-} else if (str_detect(nome_arquivo, regex("propria", ignore_case = TRUE))) {
-  base <- "propria"
-} else {
-  message("Arquivo sem equipe identificada automaticamente: ", nome_arquivo)
-  escolha <- ""
-  while (!escolha %in% c("1", "2", "3")) {
-    escolha <- readline("Selecione equipe (1-LEBATEC, 2-BRAVORE, 3-PROPRIA): ")
-  }
-  base <- c("lebatec", "bravore", "propria")[as.integer(escolha)]
-}
+> source("F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/OtimizadorIFQ6/OtimizadorIFQ6.R", encoding = 'UTF-8', echo=TRUE)
+
+> library(R6)
+
+> library(readxl)
+
+> library(dplyr)
+
+> library(tidyr)
+
+> library(purrr)
+
+> library(stringr)
+
+> library(lubridate)
+
+> library(openxlsx)
+
+> library(glue)
+
+> library(scales)
+
+> OtimizadorIFQ6 <- R6Class("OtimizadorIFQ6",
++                           public = list(
++                             validacao = function(paths) {
++ .... [TRUNCATED] 
+
+> pasta_dados <- "F:/Qualidade_Florestal/02- MATO GROSSO DO SUL/11- Administrativo Qualidade MS/00- Colaboradores/17 - Alex Vinicius/Automação em R/Ot ..." ... [TRUNCATED] 
+
+> arquivos <- list.files(
++   path       = pasta_dados,
++   pattern    = "\\.xlsx$",
++   full.names = TRUE
++ )
+
+> arquivos <- c(
++   arquivos[str_detect(toupper(basename(arquivos)), "SGF")],
++   setdiff(arquivos, arquivos[str_detect(toupper(basename(arquivos)),  .... [TRUNCATED] 
+
+> otimizador <- OtimizadorIFQ6$new()
+
+> otimizador$validacao(arquivos)
+Arquivo sem equipe identificada automaticamente: 6271_TABOCA_SRP - IFQ6 (4).xlsx
+Selecione equipe (1-LEBATEC, 2-BRAVORE, 3-PROPRIA): 1
+Error in nomes_colunas %notin% toupper(names(df)) : 
+  não foi possível encontrar a função "%notin%"
