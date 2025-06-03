083ba04abc366055419b25c8a109a43b1f8ca37a
alek <alekviniciuscorreia@gmail.com>
2025-06-03 12:22:45 -0300

    Update OtimizadorIFQ6.R

--- OtimizadorIFQ6.R
+++ OtimizadorIFQ6.R
@@ -1,35 +1,8 @@
-# 8) leitura de cadastro e criação de “Index”
-df_cadastro <- read_excel(cadastro_path, sheet = 1, col_types = "text") %>%
-  mutate(Index = paste0(`Id Projeto`, Talhao))
-
-df_final <- df_final %>%
-  mutate(
-    Index = paste0(CD_PROJETO, CD_TALHAO),
-    Index = if_else(
-      str_detect(Index, "-\\d{2}$"),
-      Index,
-      if_else(
-        str_detect(Index, "-\\d$"),
-        str_replace(Index, "-(\\d)$", "-0\\1"),
-        paste0(Index, "-01")
-      )
-    )
-  )
-
-
-area_col <- df_cadastro %>%
-  select(contains("AREA")) %>%
-  names() %>%
-  first()
-
-df_res <- df_final %>%
-  left_join(
-    df_cadastro %>% select(Index, all_of(area_col)),
-    by = "Index"
-  ) %>%
-  rename(Area_ha = !!sym(area_col)) %>%
-  mutate(Area_ha = replace_na(Area_ha, "")) %>%
-  rename(
-    nm_parcela      = NM_PARCELA,
-    nm_area_parcela = NM_AREA_PARCELA
-  )
+New names:
+* `` -> `...35`
+Quantidade de 'VERIFICAR': 0
+Error in `mutate()`:
+i In argument: `Index = paste0(`Id Projeto`, Talhao)`.
+Caused by error:
+! objeto 'Talhao' não encontrado
+Run `rlang::last_trace()` to see where the error occurred.
