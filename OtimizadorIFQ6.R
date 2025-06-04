6c98448de84060fb4e6e61af4dbc1672b644b221
alek <alekviniciuscorreia@gmail.com>
2025-06-04 10:51:56 -0400

    Update OtimizadorIFQ6.R

--- OtimizadorIFQ6.R
+++ OtimizadorIFQ6.R
@@ -1,47 +1,7 @@
-# Solução reestruturada para evitar erro com mutate()
-
-# 1. Calcular a mediana de altura por projeto e talhão
-medianas_df <- df_res %>%
-  group_by(CD_PROJETO, CD_TALHAO) %>%
-  summarize(mediana_ht_proj_tal = median(Ht_media, na.rm = TRUE), .groups = "drop")
-
-# 2. Juntar os dados e calcular métricas
-
-# unindo df_pivot com df_metrics_C
-
-df_C <- bind_cols(df_pivot, df_metrics_C) %>%
-
-  # adicionar contagens
-  left_join(
-    conts,
-    by = c("CD_PROJETO" = "CD_PROJETO",
-           "CD_TALHAO"   = "CD_TALHAO",
-           "NM_PARCELA"  = "NM_PARCELA")
-  ) %>%
-
-  # substituir NAs por zero nas colunas falt_c e falt_f
-  mutate(across(all_of(c(falt_c, falt_f)), ~ replace_na(.x, 0))) %>%
-
-  # juntar as medianas calculadas
-  left_join(medianas_df, by = c("CD_PROJETO", "CD_TALHAO")) %>%
-
-  # calcular colunas derivadas
-  mutate(
-    Stand_tree_ha = ((rowSums(across(all_of(codes))) -
-                        rowSums(across(all_of(falhas)))) * 10000) /
-      as.numeric(NM_AREA_PARCELA),
-
-    Pits_ha       = (((n - L) * 10000) / as.numeric(NM_AREA_PARCELA)),
-
-    surv_dec      = (rowSums(across(all_of(codes))) -
-                       rowSums(across(all_of(falhas)))) /
-      rowSums(across(all_of(codes))),
-
-    Percent_Sobrevivencia = percent(surv_dec, accuracy = 0.1, decimal.mark = ","),
-
-    Pits_por_sob  = Stand_tree_ha / surv_dec,
-
-    Check_pits    = Pits_por_sob - Pits_ha
-  ) %>%
-
-  select(-surv_dec)
+                                # 5) concatenação e verificações iniciais
+                                df_final <- bind_rows(lista_df) %>%
+                                  mutate(NM_COVA = as.numeric(NM_COVA)) %>%
+                                  arrange(CD_PROJETO, CD_TALHAO, NM_PARCELA, NM_FILA, NM_COVA) %>% #ele deveria toda vez que ele contasse cd_projeto,cd_talhao_nm_parcela e nm_fila toda vez que um desses mudasse ele reiniciasse a contage, indo 1,2,3,4,5,6 sequencialmente e claro que seguindo a regra da bifurcação do L.
+                                  group_by(CD_PROJETO, CD_TALHAO, NM_PARCELA, NM_FILA) %>%
+                                  mutate(NM_COVA = row_number()) %>%
+                                  ungroup()
